7bc91fdfe75edd678ad367a2c478e35e7ee1a2e3
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2024-09-22 15:20:58 +0330

    coding hamburger menu

---
+++ src/components/modules/header/Header.js
@@ -0,0 +1,178 @@
+"use client";
+import React, { useState } from 'react'
+import Link from 'next/link'
+import styles from './Header.module.css'
+
+function Header() {
+
+    const [activeHamburger, setActiveHamburger] = useState(false)
+    const [activeSubmenu,setActiveSubmenu] = useState(false)
+    return (
+        <header className={styles.header}>
+            <div className='container-fluid'>
+                <div className="row align-items-center">
+                    {/* logo */}
+                    <div className="col-lg-1 col-6 order-lg-1 order-1">
+                        <div className="d-flex align-items-center">
+                            <div className="d-block d-lg-none">
+                                <i type="button" onClick={() => setActiveHamburger(true)} class="bi bi-list font-30"></i>
+
+                                <div className={activeHamburger ? styles.hamburger_menu_show : styles.hamburger_menu_hidden}>
+                                    <div className={styles.hamburger_menu_header}>
+                                        <h5 className='hamburger_menu_title'>فروشگاه نسیم استور</h5>
+                                        <button
+                                            onClick={() => setActiveHamburger(false)}
+                                            className={`btn-close ${styles.hamburger_btn_close}`}></button>
+                                    </div>
+
+                                    <div className={styles.hamburger_body}>
+                                        <a href="" className='text-center d-block mb-3'>
+                                            <img className='img-fluid' width="200" src="/images/logo.png" alt="logo" />
+                                        </a>
+                                        <div className="hamberger_body_bottom_form">
+                                            <div className={styles.search_from}>
+                                                <form action="">
+                                                    <div className={styles.search_field}>
+                                                        <input
+                                                            placeholder='جسنجوی محصولات ...'
+                                                            type="text" className={`form-control ${styles.search_input}`} />
+                                                        <button type='submit'
+                                                            className={`btn ${styles.search_btn} main-color-one-bg rounded-pill`}
+                                                        >
+                                                            <i
+
+                                                                class="bi bi-search"></i>
+                                                        </button>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                        <ul className={`navbar-nav ${styles.hamburger_navbar}`}>
+                                            <li className={`nav-item ${styles.hamburger_navbar_item} `}>
+                                                <Link href="/" className='nav-link'>
+                                                    صفحه اصلی
+                                                </Link>
+                                            </li>
+                                            <li className={`nav-item ${styles.hamburger_navbar_item} `}>
+                                                <Link href="/" className='nav-link'>
+                                                    گوشی موبایل
+                                                </Link>
+                                                <span
+                                                onClick={() => setActiveSubmenu(!activeSubmenu)}
+                                                className={styles.hamberger_showSubMenu}>
+                                                    <i class="bi bi-chevron-left"></i>
+                                                </span>
+                                                <ul className={`navbar-nav ${activeSubmenu? styles.hamburger_navbar_submenu_show :styles.hamburger_navbar_submenu_hide}`}>
+                                                    <li className='nav-item'>
+                                                    <Link className="nav-link" href="/">برند</Link>
+                                                    <span className={styles.hamberger_showSubMenu}><i class="bi bi-chevron-left"></i></span>
+                                                    <ul class="navbar-nav h-0 bg-ul-f7">
+                                                        <li class="nav-item"><a href="" class="nav-link">سامسونگ</a>
+                                                        </li>
+                                                        <li class="nav-item"><a href="" class="nav-link">هوآوی</a></li>
+                                                        <li class="nav-item"><a href="" class="nav-link">شیائومی</a>
+                                                        </li>
+                                                        <li class="nav-item"><a href="" class="nav-link">الجی</a></li>
+                                                        <li class="nav-item"><a href="" class="nav-link">سونی</a></li>
+                                                    </ul>
+                                                    </li>
+                                                </ul>
+                                            </li>
+                                            <li className={`nav-item ${styles.hamburger_navbar_item} `}>
+                                                <Link href="/" className='nav-link'>
+                                                    تبلت
+                                                </Link>
+                                                <span className={styles.hamberger_showSubMenu}>
+                                                    <i class="bi bi-chevron-left"></i>
+                                                </span>
+                                                <ul></ul>
+                                            </li>
+                                            <li className={`nav-item ${styles.hamburger_navbar_item} `}>
+                                                <Link href="/" className='nav-link'>
+                                                    لپتاپ
+                                                </Link>
+                                                <span className={styles.hamberger_showSubMenu}>
+                                                    <i class="bi bi-chevron-left"></i>
+                                                </span>
+                                                <ul></ul>
+                                            </li>
+                                            <li className={`nav-item ${styles.hamburger_navbar_item} `}>
+                                                <Link href="/" className='nav-link'>
+                                                    صفحات
+                                                </Link>
+                                                <span className={styles.hamberger_showSubMenu}>
+                                                    <i class="bi bi-chevron-left"></i>
+                                                </span>
+                                                <ul></ul>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div className={activeHamburger ? styles.hamburger_backdrop : styles.hamburger_backdrop_hidden} ></div>
+                            </div>
+                            <div >
+                                <Link href='/'>
+                                    <div className="logo">
+                                        <img className='img-fluid' src="/images/logo.png" alt="logo" />
+                                    </div>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                    {/* end logo */}
+
+                    {/* action */}
+                    <div className="d-lg-none d-block col-6 order-lg-5 order-2 ">hi2</div>
+                    {/* end action */}
+
+
+                    {/* form search */}
+                    <div className="col-lg-7 order-lg-2 d-lg-block d-none">
+                        <div className="search-form">
+                            <form action="">
+                                <div className={styles.search_field}>
+                                    <input type="text" placeholder='جستجوی محصولات ...' className={`form-control ${styles.search_input}`} />
+                                    <button type='button' className={`btn main-color-one-bg ${styles.search_btn} rounded-pill`}>
+                                        <i className='bi bi-search'></i>
+
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    {/* end form search */}
+
+
+                    {/* auth */}
+                    <div className="col-lg-4 order-lg-3 d-lg-block d-none">
+                        <div className="d-flex align-items-center justify-content-end">
+                            <div className={`btn-group rounded-pill shadow-sm ${styles.header_btn_group} `}>
+                                <div>
+                                    <Link href='/register' className={`${styles.header_register} btn btn-white border-o rounded-pill`}>
+                                        <i className='bi bi-person-circle font-20 text-muted me-1'></i>
+                                        <span>ثبت نام</span>
+                                    </Link>
+                                </div>
+                                <Link href="/login" className={`${styles.header_login} btn btn-white border-o rounded-pill`}>ورود</Link>
+                            </div>
+                            <div className={`${styles.header_whishlist} shadow-sm`}>
+                                <Link href='/wishlist'>
+                                    <i className='bi bi-bookmark-check font-20 text-muted'></i>
+                                </Link>
+                            </div>
+                            <div className={`${styles.header_cart} ms-1 shadow-sm rounded-pill`}>
+                                <Link href='' className={` btn header-cart-toggle  btn-white border-0 `}>
+                                    <i className='bi bi-shop font-20 text-muted me-1'></i>
+                                    <span className={`main-color-one-bg ${styles.header_cart_counter} rounded-pill`}>5</span>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                    {/* end auth */}
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header
