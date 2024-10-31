5366edb96db47632f39a5f5575ab092c4e36bed9
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2024-10-31 18:40:32 +0330

    coding slider for image product

--- src/components/templates/product/product-images-slider/ImageSlider.js
+++ src/components/templates/product/product-images-slider/ImageSlider.js
@@ -1,54 +1,76 @@
-// "use client";
-// import './product-image-slider.scss'
-// import PropTypes from 'prop-types'
-// import { Swiper, SwiperSlide } from 'swiper/react'
-// import { Navigation, Thumbs } from 'swiper'
-// import { useState } from 'react'
+"use client";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import "swiper/css/thumbs";
+import "swiper/css/pagination"
+import { FreeMode, Navigation, Thumbs, Pagination } from "swiper/modules";
 
-// const ProductImagesSlider = props => {
-//     const [activeThumb, setActiveThumb] = useState()
+import styles from '../ImageSlider.module.css'
+import { useState } from "react";
 
-//     return <>
-//         <Swiper
-//             loop={true}
-//             spaceBetween={10}
-//             navigation={true}
-//             modules={[Navigation, Thumbs]}
-//             grabCursor={true}
-//             thumbs={{ swiper: activeThumb }}
-//             className='product-images-slider'
-//         >
-//             {
-//                 props.images.map((item, index) => (
-//                     <SwiperSlide key={index}>
-//                         <img src={item} alt="product images" />
-//                     </SwiperSlide>
-//                 ))
-//             }
-//         </Swiper>
-//         <Swiper
-//             onSwiper={setActiveThumb}
-//             loop={true}
-//             spaceBetween={10}
-//             slidesPerView={4}
-//             modules={[Navigation, Thumbs]}
-//             className='product-images-slider-thumbs'
-//         >
-//             {
-//                 props.images.map((item, index) => (
-//                     <SwiperSlide key={index}>
-//                         <div className="product-images-slider-thumbs-wrapper">
-//                             <img src={item} alt="product images" />
-//                         </div>
-//                     </SwiperSlide>
-//                 ))
-//             }
-//         </Swiper>
-//     </>
-// }
+const Gallery = () => {
+    const [thumbsSwiper, setThumbsSwiper] = useState(null);
+    const images = [
+        "/images/product/laptop-1.jpg",
+        "/images/product/laptop-2.jpg",
+        "/images/product/laptop-3.jpg",
+        "/images/product/laptop-4.jpg",
+        "/images/product/laptop-5.jpg",
+        "/images/product/laptop2.jpg",
 
-// ProductImagesSlider.propTypes = {
-//     images: PropTypes.array.isRequired
-// }
+    ];
 
-// export default ProductImagesSlider
+    return (
+        <section style={{ width: "100%" }}>
+            <Swiper
+                style={{
+                    "--swiper-navigation-color": "#fff",
+                    "--swiper-pagination-color": "#fff",
+                }}
+                spaceBetween={10}
+                navigation={{
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev'
+                }}
+                thumbs={{ swiper: thumbsSwiper }}
+                pagination={{
+                    clickable: true,
+                    bulletClass: `swiper-pagination-bullet ${styles.customBullet}`,
+                    bulletActiveClass: `swiper-pagination-bullet-active ${styles.customBulletActive}`,
+                }}
+                modules={[FreeMode, Navigation, Thumbs, Pagination]}
+                className="mySwiper2 gallery-slider"
+            >
+                {images.map((img, index) => (
+                    <SwiperSlide key={index} className={styles.swiper_slide}>
+                        <div className={styles.swiper_zoom_container}>
+                            <img alt="" className="img-fluid" src={img} />
+                        </div>
+                    </SwiperSlide>
+                ))}
+                <div className={`${styles.swiper_button_next} swiper-button-next`}></div>
+                <div className={`${styles.swiper_button_prev} swiper-button-prev`}></div>
+            </Swiper>
+
+            <Swiper
+                onSwiper={setThumbsSwiper}
+                spaceBetween={10}
+                slidesPerView={4}
+                freeMode={true}
+                watchSlidesProgress={true}
+                modules={[FreeMode, Navigation, Thumbs]}
+                className="gallery-slider-2"
+            >
+                {images.map((img, index) => (
+                    <SwiperSlide key={index} className={styles.product_gallery_thumb}>
+                        <img alt="" className="img-fluid" src={img} />
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </section>
+    );
+};
+
+export default Gallery;
