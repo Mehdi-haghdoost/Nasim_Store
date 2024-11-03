66256a8a936038a0430f7d68c3a27c692aae3411
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2024-11-03 15:30:54 +0330

    coding Descriptions component mode

--- src/components/templates/product/Descriptions.js
+++ src/components/templates/product/Descriptions.js
@@ -1,8 +1,52 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import styles from './Descriptions.module.css'
 function Descriptions() {
+
+  const [isPlayVideo, setIsPlayVideo] = useState(false);
+
+
+  const videoRef = useRef(null);
+
+  const playVideoHandler = () => {
+    setIsPlayVideo(prev => !prev);
+    if (videoRef.current) {
+      if (isPlayVideo) {
+        videoRef.current.pause(); // توقف ویدیو
+      } else {
+        videoRef.current.play(); // پخش ویدیو
+      }
+    }
+  }
   return (
-    <div>Descriptions</div>
+    <>
+      <h6>معرفی محصول</h6>
+      <div className="row gy-3">
+        <div className="col-lg-6">
+
+        </div>
+        <div className="col-lg-6">
+          <div className={styles.video}>
+            <video
+              ref={videoRef}
+              poster="/images/product/laptop-1.jpg"
+              loop preload="none" className={`img-fluid ${styles.vd_style}`} src="/video/video.mp4">
+              <source
+                src="/video/video.mp4"
+                type="video/mp4"
+              />
+            </video>
+            <div className={`${styles.play_btn} pointer ${isPlayVideo ? `${styles.play_opacity}` : "active"}`}>
+              <div
+                className={styles.play}
+                onClick={playVideoHandler}
+              >
+                <i className={`bi ${isPlayVideo ? "bi-pause-fill" : "bi-play-fill"}`} id="play-icon"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
