481db7e89a0dd3948b728c1d8878a60cee80d051
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2024-11-04 17:33:41 +0330

    coding Information component mode

--- src/components/templates/product/Informations.js
+++ src/components/templates/product/Informations.js
@@ -2,7 +2,227 @@ import React from 'react'
 import styles from './Informations.module.css'
 function Informations() {
   return (
-    <div>additional information</div>
+    <>
+      <h6 className={styles.product_desc_tab_title}>مشخصات فنی</h6>
+      <div className={`mt-4 ${styles.box_list}`}>
+        <p className={styles.box_list_title}>
+          <svg xmlns="http://www.w3.org/2000/svg"
+            width="16" height="16" fill="currentColor"
+            class="bi bi-caret-left-fill" viewBox="0 0 16 16">
+            <path
+              d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z">
+            </path>
+          </svg>
+          مشخصات کلی
+        </p>
+        <div>
+          <ul className={`list-inline ${styles.param_list}`}>
+            <li className='list-inline-item col-md-3 pe-md-1 pe-md-3 p-0 m-0'>
+              <div className={styles.box_params_list}>
+                <p className='block border_right_custom1'>
+                  قابلیت پخش موسیقی
+                </p>
+              </div>
+            </li>
+            <li className='list-inline-item col-md-8 p-0 m-0'>
+              <div className={styles.box_params_list}>
+                <p className='block border_right_custom2'>دارد</p>
+              </div>
+            </li>
+
+            <li className='list-inline-item col-md-3 pe-md-1 pe-md-3 p-0 m-0'>
+              <div className={styles.box_params_list}>
+                <p className='block border_right_custom1'>
+                  قابلیت کنترل صدا و موزیک
+                </p>
+              </div>
+            </li>
+            <li className='list-inline-item col-md-8 p-0 m-0'>
+              <div className={styles.box_params_list}>
+                <p className='block border_right_custom2'>دارد</p>
+              </div>
+            </li>
+
+            <li className='list-inline-item col-md-3 pe-md-1 pe-md-3 p-0 m-0'>
+              <div className={styles.box_params_list}>
+                <p className='block border_right_custom1'>
+                  راهنمای صوتی
+                </p>
+              </div>
+            </li>
+            <li className='list-inline-item col-md-8 p-0 m-0'>
+              <div className={styles.box_params_list}>
+                <p className='block border_right_custom2'>ندارد</p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div className={`mt-4 ${styles.box_list}`}>
+        <p className={styles.box_list_title}>
+          <svg xmlns="http://www.w3.org/2000/svg"
+            width="16" height="16" fill="currentColor"
+            class="bi bi-caret-left-fill" viewBox="0 0 16 16">
+            <path
+              d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z">
+            </path>
+          </svg>
+          مشخصات کلی
+        </p>
+        <div>
+          <ul className={`list-inline ${styles.param_list}`}>
+            <li className='list-inline-item col-md-3 pe-md-1 pe-md-3 p-0 m-0'>
+              <div className={styles.box_params_list}>
+                <p className='block border_right_custom1'>
+                  قابلیت پخش موسیقی
+                </p>
+              </div>
+            </li>
+            <li className='list-inline-item col-md-8 p-0 m-0'>
+              <div className={styles.box_params_list}>
+                <p className='block border_right_custom2'>دارد</p>
+              </div>
+            </li>
+
+            <li className='list-inline-item col-md-3 pe-md-1 pe-md-3 p-0 m-0'>
+              <div className={styles.box_params_list}>
+                <p className='block border_right_custom1'>
+                  قابلیت کنترل صدا و موزیک
+                </p>
+              </div>
+            </li>
+            <li className='list-inline-item col-md-8 p-0 m-0'>
+              <div className={styles.box_params_list}>
+                <p className='block border_right_custom2'>دارد</p>
+              </div>
+            </li>
+
+            <li className='list-inline-item col-md-3 pe-md-1 pe-md-3 p-0 m-0'>
+              <div className={styles.box_params_list}>
+                <p className='block border_right_custom1'>
+                  راهنمای صوتی
+                </p>
+              </div>
+            </li>
+            <li className='list-inline-item col-md-8 p-0 m-0'>
+              <div className={styles.box_params_list}>
+                <p className='block border_right_custom2'>ندارد</p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div className={`mt-4 ${styles.box_list}`}>
+        <p className={styles.box_list_title}>
+          <svg xmlns="http://www.w3.org/2000/svg"
+            width="16" height="16" fill="currentColor"
+            class="bi bi-caret-left-fill" viewBox="0 0 16 16">
+            <path
+              d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z">
+            </path>
+          </svg>
+          مشخصات کلی
+        </p>
+        <div>
+          <ul className={`list-inline ${styles.param_list}`}>
+            <li className='list-inline-item col-md-3 pe-md-1 pe-md-3 p-0 m-0'>
+              <div className={styles.box_params_list}>
+                <p className='block border_right_custom1'>
+                  قابلیت پخش موسیقی
+                </p>
+              </div>
+            </li>
+            <li className='list-inline-item col-md-8 p-0 m-0'>
+              <div className={styles.box_params_list}>
+                <p className='block border_right_custom2'>دارد</p>
+              </div>
+            </li>
+
+            <li className='list-inline-item col-md-3 pe-md-1 pe-md-3 p-0 m-0'>
+              <div className={styles.box_params_list}>
+                <p className='block border_right_custom1'>
+                  قابلیت کنترل صدا و موزیک
+                </p>
+              </div>
+            </li>
+            <li className='list-inline-item col-md-8 p-0 m-0'>
+              <div className={styles.box_params_list}>
+                <p className='block border_right_custom2'>دارد</p>
+              </div>
+            </li>
+
+            <li className='list-inline-item col-md-3 pe-md-1 pe-md-3 p-0 m-0'>
+              <div className={styles.box_params_list}>
+                <p className='block border_right_custom1'>
+                  راهنمای صوتی
+                </p>
+              </div>
+            </li>
+            <li className='list-inline-item col-md-8 p-0 m-0'>
+              <div className={styles.box_params_list}>
+                <p className='block border_right_custom2'>ندارد</p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className={`mt-4 ${styles.box_list}`}>
+        <p className={styles.box_list_title}>
+          <svg xmlns="http://www.w3.org/2000/svg"
+            width="16" height="16" fill="currentColor"
+            class="bi bi-caret-left-fill" viewBox="0 0 16 16">
+            <path
+              d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z">
+            </path>
+          </svg>
+          مشخصات کلی
+        </p>
+        <div>
+          <ul className={`list-inline ${styles.param_list}`}>
+            <li className='list-inline-item col-md-3 pe-md-1 pe-md-3 p-0 m-0'>
+              <div className={styles.box_params_list}>
+                <p className='block border_right_custom1'>
+                  قابلیت پخش موسیقی
+                </p>
+              </div>
+            </li>
+            <li className='list-inline-item col-md-8 p-0 m-0'>
+              <div className={styles.box_params_list}>
+                <p className='block border_right_custom2'>دارد</p>
+              </div>
+            </li>
+
+            <li className='list-inline-item col-md-3 pe-md-1 pe-md-3 p-0 m-0'>
+              <div className={styles.box_params_list}>
+                <p className='block border_right_custom1'>
+                  قابلیت کنترل صدا و موزیک
+                </p>
+              </div>
+            </li>
+            <li className='list-inline-item col-md-8 p-0 m-0'>
+              <div className={styles.box_params_list}>
+                <p className='block border_right_custom2'>دارد</p>
+              </div>
+            </li>
+
+            <li className='list-inline-item col-md-3 pe-md-1 pe-md-3 p-0 m-0'>
+              <div className={styles.box_params_list}>
+                <p className='block border_right_custom1'>
+                  راهنمای صوتی
+                </p>
+              </div>
+            </li>
+            <li className='list-inline-item col-md-8 p-0 m-0'>
+              <div className={styles.box_params_list}>
+                <p className='block border_right_custom2'>ندارد</p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
   )
 }
 
