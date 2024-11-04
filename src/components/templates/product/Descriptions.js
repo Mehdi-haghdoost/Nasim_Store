ab90fae62ea8b7f41e613119dce8372e61587e96
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2024-11-04 16:01:00 +0330

    continue development of the Description component

--- src/components/templates/product/Descriptions.js
+++ src/components/templates/product/Descriptions.js
@@ -3,6 +3,7 @@ import styles from './Descriptions.module.css'
 function Descriptions() {
 
   const [isPlayVideo, setIsPlayVideo] = useState(false);
+  const [isShowMore, setIsShowMore] = useState(false);
 
 
   const videoRef = useRef(null);
@@ -19,10 +20,35 @@ function Descriptions() {
   }
   return (
     <>
-      <h6>معرفی محصول</h6>
+      <h6 className={styles.product_desc_tab_title}>معرفی محصول</h6>
       <div className="row gy-3">
         <div className="col-lg-6">
-
+          <p className={styles.product_desc_content_text}>
+            اگر به دنبال تهیه یک هدفون بی‌سیم گیمینگ هستید،
+            هدفون
+            بلوتوثی مدل K55
+            به‌عنوان
+            یکی از جدیدترین گزینه‌های موجود در بازار ارزش بررسی
+            را
+            دارد. این مدل با
+            ابعادی
+            کوچک تولید شده است. ابعاد و وزن کم آن، جابه‌جایی این
+            وسیله و استفاده
+            طولانی‌مدت
+            از آن را آسان می‌کند و باعث خستگی گوش‌ها نخواهد شد.
+            این
+            مدل برای اتصال
+            به
+            دستگاه‌ به بلوتوث نسخه 5.0 مجهز شده است و در مدت
+            زمان
+            اندکی، اتصال با
+            گوشی
+            موبایل اندروید یا ios شما برقرار خواهد شد. هدفون بی
+            سیم
+            K55 دارای یک
+            میکروفون با
+            کیفیت است و ...
+          </p>
         </div>
         <div className="col-lg-6">
           <div className={styles.video}>
@@ -44,6 +70,34 @@ function Descriptions() {
               </div>
             </div>
           </div>
+        </div>
+        <div 
+        onClick={() => setIsShowMore(prev => !prev)}
+        className={styles.product_desc_read_more}>
+          {isShowMore ? (
+            <label className='mb-2'>
+            بستن -
+          </label>
+          ) : (
+            <label className='mb-2'>
+            بیشتر +
+          </label>
+          )}
+          {isShowMore && (
+            <p>
+              و این ویژگی، آن را برای مکالمه مناسب می‌سازد. از
+              ویژگی‌های
+              اصلی این محصول محفظه نگهدارنده یا همان کیس آن است. این
+              کیس
+              قابلیت شارژ هدفون را دارد. برای شارژ آن کافی است از
+              درگاه
+              USB-C تعبیه شده بر روی کیس و کابل شارژ درون بسته‌بندی
+              استفاده کنید. گفتنی است باتری 400 میلی‌آمپر ساعتی این
+              هدفون
+              در حدود 4-6 ساعت مکالمه و 4-6 ساعت پخش موسیقی را پاسخ‌گو
+              خواهد بود.
+            </p>
+          )}
         </div>
       </div>
     </>
