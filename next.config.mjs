66e3bdeb89a8462448bcc6d71b285f613798bf49
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2024-11-12 18:10:56 +0330

    coding 503 component

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,26 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     experimental: {
+//         appDir: true,
+//     },
+// };
+
+// export default nextConfig;
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
         appDir: true,
     },
-};
-
+    async redirects() {
+        return [
+            {
+                source: '/some-path',
+                destination: '/unavailable',
+                permanent: false,
+            }
+        ]
+    }
+}
 export default nextConfig;
