4eb968381c343a5998c85ffaa423fc57bb8d71a2
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2024-11-16 07:21:31 +0330

    Add registration validation schema using Yup - Implemented registerSchema to validate user registration form fields. - Added validation for name, email, and password fields with specific constraints: - Name must be between 3 and 12 characters. - Email must be a valid email address and between 10 and 30 characters. - Password must include at least one uppercase letter, one lowercase letter, one number, and one special character. - Integrated error messages for better user feedback.

--- src/utils/register.js
+++ src/utils/register.js
@@ -1,30 +1,18 @@
 import * as Yup from "yup";
 
-const emailSchema = Yup.string()
-    .email('ایمیل معتبر نیست')
-    .required('لطفا ایمیل خود را وارد نمایید')
-
-const phoneSchema = Yup.string()
-    .matches(/^9\d{9}$/, 'شماره موبایل معتبر نیست')
-    .required("لطفا شماره تلفن خود را وارد نمایید")
-
 const registerSchema = Yup.object().shape({
-    phoneOrEmail: Yup.string()
-        .test('is-phone-or-email', 'ورودی باید ایمیل یا شماره موبایل باشد', function (value) {
-            const { path, createError } = this; return (
-                emailSchema.isValidSync(value) ||
-                phoneSchema.isValidSync(value) ||
-                createError({ path, message: 'ورودی باید ایمیل یا شماره موبایل باشد' })
-            );
-        })
+    name: Yup.string()
+        .min(3, "نام حداقل باید 3 کاراکتر داشته باشد")
+        .max(12, "نام حداکثر باید 12 کاراکتر داشته باشد")
         .required('ورودی مورد نیاز است'),
+    email: Yup.string()
+        .required('لطفا ایمیل خود را وارد نمایید')
+        .matches(/[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/g, 'لطفا ایمیل معتبر وارد نمایید')
+        .min(10, 'ایمیل حداقل باید 10 کاراکتر داشه باشد')
+        .max(30, 'ایمیل حداکثر باید 30 کاراکتر داشته باشد'),
     password: Yup.string()
-        .min(8, 'پسورد باید حداقل 8 کاراکتر باشد')
-        .matches(/[a-z]/, 'پسورد باید حداقل یک حرف کوچک داشته باشد').matches(/[A-Z]/,
-            'پسورد باید حداقل یک حرف بزرگ داشته باشد')
-        .matches(/[0-9]/, 'پسورد باید حداقل یک عدد داشته باشد')
-        .matches(/[@$!%*?&]/, 'پسورد باید حداقل یک کاراکتر خاص داشته باشد')
-        .required('پسورد مورد نیاز است'),
+        .required('لطفا رمز عبور خود را وارد کنید')
+        .matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/g, 'رمز عبور معتبر وارد نمایید')
 });
 
 export default registerSchema;
