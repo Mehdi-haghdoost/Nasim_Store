00026a2afbe243716491ca170a6eaf287f4ffec3
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2024-11-17 17:41:21 +0330

    coding Box component

---
+++ src/components/templates/p-user/index/Box.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import styles from './box.module.css';
+import Link from 'next/link';
+
+function Box({title,iconClass}) {
+  return (
+    <div className='col-lg-3 col-6 w-100-in-400'>
+        <Link href={"/p-user"}>
+        <div className="content-box">
+            <div className="container-fluid">
+                <div className="item">
+                    <div className="row align-items-center">
+                        <div className="col-2">
+                            <div className={styles.icon}>
+                                <i className={`bi ${iconClass} main-color-one-color font-30`}></i>
+                            </div>
+                        </div>
+                        <div className="col-10">
+                            <h6 className='font-16 mb-0'>
+                                {title}
+                                <span className='badge rounded-pill badge-spn me-3'>5</span>
+                            </h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </Link>
+    </div>
+  )
+}
+
+export default Box
