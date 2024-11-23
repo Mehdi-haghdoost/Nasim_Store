d98ac49192ae8eac1ed2b0cb643dfc8e4666d7bf
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2024-11-23 07:04:11 +0330

    coding DataTable for userInfo in panel-user

--- src/components/templates/p-user/profile/DataTable.js
+++ src/components/templates/p-user/profile/DataTable.js
@@ -3,8 +3,47 @@ import styles from './DataTable.module.css';
 const DataTable = () => {
 
     return (
-        <table className={`${table} main-table shadow-one main-table-2`}></table>
+        <table className={`${styles.table} main-table shadow-one main-table-2`}>
+            <tbody>
+                <tr>
+                    <td className="text-start p-3">
+                        <h6>نام و نام خانوادگی</h6>
+                        <p className="mt-2">مهدی حق دوست</p>
+                    </td>
+                    <td className="text-start p-3">
+                        <h6>شماره تلفن</h6>
+                        <p className="mt-2">09211367465</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td className="text-start p-3">
+                        <h6>پست الکترونیک</h6>
+                        <p className="mt-2">mahdi.leo2014@gmail.com</p>
+                    </td>
+                    <td className="text-start p-3">
+                        <h6>کد ملی </h6>
+                        <p className="mt-2">00146564664664</p>
+                    </td>
+                </tr>
+                <tr>
+                <td className="text-start p-3">
+                        <h6>عضویت </h6>
+                        <p className="mt-2">4 آذر 1403</p>
+                    </td>
+                    <td className="text-start p-3">
+                        <h6>کد پستی </h6>
+                        <p className="mt-2">3321546987</p>
+                    </td>
+                </tr>
+                <tr>
+                <td className="text-start p-3">
+                        <h6>آدرس </h6>
+                        <p className="mt-2"> تهران اندیشه فاز 3 انتهای خیابان بنفشه پلاک 0</p>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     )
 }
 
-export default DataTable
+export default DataTable;
