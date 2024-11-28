043e1e90c807b3c83740032c16f7cc5224aae4f0
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2024-11-29 02:54:47 +0330

    edit and enhance tickets component

--- src/components/templates/p-user/tickets/Tickets.js
+++ src/components/templates/p-user/tickets/Tickets.js
@@ -8,11 +8,10 @@ const Tickets = () => {
             <table className="table shadow-none table-bordered">
                 <thead>
                     <tr>
-                        <th className='align-middle text-center'> <h6 className="font-18 text-muteLink">شناسه/p-user/tickets/</h6></th>
+                        <th className='align-middle text-center'> <h6 className="font-18 text-muteLink">شناسه</h6></th>
                         <th className='align-middle text-center'> <h6 className="font-18 text-muted">عنوان</h6></th>
+                        <th className='align-middle text-center'> <h6 className="font-18 text-muted">تاریخ بروز رسانی</h6></th>
                         <th className='align-middle text-center'> <h6 className="font-18 text-muted">نمایش</h6></th>
-                        <th className='align-middle text-center'> <h6 className="font-18 text-muted">پاسخ</h6></th>
-                        <th className='align-middle text-center'> <h6 className="font-18 text-muted">بن</h6></th>
                     </tr>
                 </thead>
                 <tbody>
@@ -26,31 +25,7 @@ const Tickets = () => {
                             </p>
                         </td>
                         <td className="align-middle text-center">
-                            <Link href="/p-user/tickets/ticket-message" className='btn main-color-three-bg shadow-none btn-sm'>
-                                <i className="bi bi-eye ms-1 text-white"></i>
-                                نمایش
-                            </Link>
-                        </td>
-                        <td className="align-middle text-center">
-                            <a href="" className='btn bg-success text-white shadow-none btn-sm'>
-                                <i className="bi bi-eye ms-1 text-white"></i>
-                                پاسخ
-                            </a>
-                        </td>
-                        <td className="align-middle text-center">
-                            <a href="" className='btn bg-danger text-white shadow-none btn-sm'>
-                                <i className="bi bi-eye ms-1 text-white"></i>
-                                بن
-                            </a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td className="align-middle text-center"><p className="mt-2 font-16">545315</p></td>
-                        <td className="align-middle text-center">
-                            <p className="mt-2 font-16">ارسال نشدن فلان سفارش برای من
-                                <span className="badge bg-success rounded-0 me-2 text-white">
-                                    پاسخ داده شده
-                                </span>
+                            <p className="mt-2 font-16">9 آذر 1403 ساعت 02:53
                             </p>
                         </td>
                         <td className="align-middle text-center">
@@ -59,26 +34,18 @@ const Tickets = () => {
                                 نمایش
                             </Link>
                         </td>
-                        <td className="align-middle text-center">
-                            <a href="" className='btn bg-success text-white shadow-none btn-sm'>
-                                <i className="bi bi-eye ms-1 text-white"></i>
-                                پاسخ
-                            </a>
-                        </td>
-                        <td className="align-middle text-center">
-                            <a href="" className='btn bg-danger text-white shadow-none btn-sm'>
-                                <i className="bi bi-eye ms-1 text-white"></i>
-                                بن
-                            </a>
-                        </td>
                     </tr>
                     <tr>
                         <td className="align-middle text-center"><p className="mt-2 font-16">545315</p></td>
                         <td className="align-middle text-center">
                             <p className="mt-2 font-16">ارسال نشدن فلان سفارش برای من
-                                <span className="badge bg-danger rounded-0 me-2 text-white">
-                                    بسته شده
+                                <span className="badge bg-secondary rounded-0 me-2 text-white">
+                                    در انتظار پاسخ
                                 </span>
+                            </p>
+                        </td>
+                        <td className="align-middle text-center">
+                            <p className="mt-2 font-16">9 آذر 1403 ساعت 02:53
                             </p>
                         </td>
                         <td className="align-middle text-center">
@@ -87,17 +54,25 @@ const Tickets = () => {
                                 نمایش
                             </Link>
                         </td>
+                    </tr>
+                    <tr>
+                        <td className="align-middle text-center"><p className="mt-2 font-16">545315</p></td>
                         <td className="align-middle text-center">
-                            <a href="" className='btn bg-success text-white shadow-none btn-sm'>
-                                <i className="bi bi-eye ms-1 text-white"></i>
-                                پاسخ
-                            </a>
+                            <p className="mt-2 font-16">ارسال نشدن فلان سفارش برای من
+                                <span className="badge bg-secondary rounded-0 me-2 text-white">
+                                    در انتظار پاسخ
+                                </span>
+                            </p>
                         </td>
                         <td className="align-middle text-center">
-                            <a href="" className='btn bg-danger text-white shadow-none btn-sm'>
+                            <p className="mt-2 font-16">9 آذر 1403 ساعت 02:53
+                            </p>
+                        </td>
+                        <td className="align-middle text-center">
+                            <Link href="/p-user/tickets/ticket-message" className='btn main-color-three-bg shadow-none btn-sm'>
                                 <i className="bi bi-eye ms-1 text-white"></i>
-                                بن
-                            </a>
+                                نمایش
+                            </Link>
                         </td>
                     </tr>
                 </tbody>
