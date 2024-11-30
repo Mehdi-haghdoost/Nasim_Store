ec51b094c30820083a618628ff693d1ace2e300e
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2024-11-30 13:33:27 +0330

    Initialize structure for seller panel - Created base directories and components for seller dashboard - Set up foundational layout and routing

--- src/app/p-seller/page.js
+++ src/app/p-seller/page.js
@@ -1,11 +1,26 @@
-import Layout from '@/components/layouts/SellerPanelLayout'
 import React from 'react'
+import styles from '@/styles/p-seller/index.module.css';
+import Layout from '@/components/layouts/SellerPanelLayout'
+import PanelHeader from '@/components/templates/p-seller/index/PanelHeader';
+import Box from '@/components/templates/p-seller/index/Box';
+import SellerTable from '@/components/templates/p-seller/index/SellerTable';
 
 const page = () => {
     return (
         <Layout>
             <main>
-                <div className="position-sticky top-0"></div>
+                <div className="position-sticky top-0">
+                    <PanelHeader />
+                    <div className={`${styles.status_panel_seller} my-5`}>
+                        <Box />
+                        <Box />
+                        <Box />
+                        <Box />
+                    </div>
+                    <div className={styles.panel_latest_order}>
+                        <SellerTable />
+                    </div>
+                </div>
             </main>
         </Layout>
     )
