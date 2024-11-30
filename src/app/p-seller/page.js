869081afbd4391a4e4dd44937c57060f001b3cc3
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2024-11-30 12:34:18 +0330

    Create seller panel under /p-seller  - Initialized basic structure for the seller dashboard

---
+++ src/app/p-seller/page.js
@@ -0,0 +1,14 @@
+import Layout from '@/components/layouts/SellerPanelLayout'
+import React from 'react'
+
+const page = () => {
+    return (
+        <Layout>
+            <main>
+                <div className="position-sticky top-0"></div>
+            </main>
+        </Layout>
+    )
+}
+
+export default page
