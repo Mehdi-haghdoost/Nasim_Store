f1a5add5c0f14f8bc12da1ab781fd274b6c74fa9
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2024-11-30 13:33:07 +0330

    coding box component in seller panel

--- src/components/templates/p-seller/index/Box.js
+++ src/components/templates/p-seller/index/Box.js
@@ -1,9 +1,24 @@
 import React from 'react'
 import styles from './Box.module.css';
+import Link from 'next/link';
 
 const Box = () => {
     return (
-        <div>Box</div>
+        <div className="row g-3">
+            <div className="col-md-3 col-md-6">
+                <Link href={"/"}>
+                    <div className={`${styles.panel_seller_item} d-flex align-items-center`}>
+                        <div className={styles.panel_seller_icon}>
+                            <i className="bi bi-bag-check"></i>
+                        </div>
+                        <div className={`d-flex flex-column ${styles.panel_seller_title}`}>
+                            <h6 className="font-14">سفارشات تکمیل شده</h6>
+                            <h5 className="title-font h3"></h5>
+                        </div>
+                    </div>
+                </Link>
+            </div>
+        </div>
     )
 }
 
