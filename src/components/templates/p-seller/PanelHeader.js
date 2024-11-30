0a8562ad14cb6aa86d8725592d9ce81b3eeab679
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2024-11-30 12:42:03 +0330

    create paneleader section

---
+++ src/components/templates/p-seller/PanelHeader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './PanelHeader.module.css';
+
+const PanelHeader = () => {
+  return (
+    <div className={styles.panel_header}>
+        
+    </div>
+  )
+}
+
+export default PanelHeader
