761fd968bbd79c601185c9bc60c9f4bd169f6384
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2024-12-05 16:34:22 +0330

    styling BlogItem component

--- src/components/templates/blog/blog-lastVideos/BlogItem.js
+++ src/components/templates/blog/blog-lastVideos/BlogItem.js
@@ -5,13 +5,13 @@ import styles from './BlogItem.module.css';
 const BlogItem = () => {
   return (
     <li className="nav-item">
-    <Link href={"/blog"}>
+    <Link href={"/blog"} className='nav-link'>
         <div className={styles.video_blog_item}>
             <div className={styles.image}>
                 <img src="/images/blog-6.jpg" className='img-fluid' alt="" />
             </div>
             <div className={`d-flex align-items-start flex-wrap flex-column justify-content-between ${styles.desc}`}>
-                <h5 className='title'>آخرین پرچمداران گوشی شیائومی</h5>
+                <h5 className={styles.title}>آخرین پرچمداران گوشی شیائومی</h5>
                 <div className="d-flex align-items-center">
                     <p className="text-muted mb-0">13 آذر ماه 1403</p>
                     <div className="text-end me-2">
