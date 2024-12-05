2081a6c753d9b883a4c5e591d0f569238d9d1696
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2024-12-05 17:15:03 +0330

    Refactored BlogLastPosts Cart component and updated corresponding styles in Cart.module.css for better structure and styling consistency

--- src/components/templates/blog/blog-lastPosts/Cart.js
+++ src/components/templates/blog/blog-lastPosts/Cart.js
@@ -4,27 +4,29 @@ import Link from 'next/link';
 
 const Cart = () => {
     return (
-        <div className={styles.blog_item}>
-            <Link href={'/'}>
-                <div className={styles.image}>
-                    <img src="/images/blog-1.jpg" alt="blog_item" className='img-fluid' />
-                </div>
-                <div className={styles.title}>
-                    <div className='d-flex align-items-center justify-content-between'>
-                        <div className={styles.title_item}>
-                            <i className="bi bi-tag ms-1"></i>
-                            <span className="font-12">آموزشی</span>
-                        </div>
-                        <div className={styles.title_item}>
-                            <i className="bi bi-alarm ms-1"></i>
-                            <span className="font-12">15 آذر 1403</span>
-                        </div>
+        <div className="col-lg-3">
+            <div className={styles.blog_item}>
+                <Link href={'/'}>
+                    <div className={styles.image}>
+                        <img src="/images/blog-1.jpg" alt="blog_item" className='img-fluid' />
                     </div>
-                    <h4 className="font-16 text-overflow-1 h4">
-                        ارز دیجیتال چیست و چگونه استخراج میشود؟
-                    </h4>
-                </div>
-            </Link>
+                    <div className={styles.title}>
+                        <div className='d-flex align-items-center justify-content-between'>
+                            <div className={styles.title_item}>
+                                <i className="bi bi-tag ms-1"></i>
+                                <span className="font-12">آموزشی</span>
+                            </div>
+                            <div className={styles.title_item}>
+                                <i className="bi bi-alarm ms-1"></i>
+                                <span className="font-12">15 آذر 1403</span>
+                            </div>
+                        </div>
+                        <h4 className="font-16 text-overflow-1 h4">
+                            ارز دیجیتال چیست و چگونه استخراج میشود؟
+                        </h4>
+                    </div>
+                </Link>
+            </div>
         </div>
 
 
