d24bad79843f78ee7f2f0110e4586b951b3abbd6
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2024-12-23 17:14:34 +0330

    Continued development of the product compare page.

--- src/app/compare/page.js
+++ src/app/compare/page.js
@@ -3,6 +3,7 @@ import styles from '@/styles/compare.module.css';
 import BreadCroumb from '@/components/modules/breadCroumb/BreadCroumb';
 import Footer from '@/components/modules/footer/Footer';
 import Header from '@/components/modules/header/Header';
+import Link from 'next/link';
 
 
 
@@ -28,7 +29,7 @@ const page = () => {
                         <div className="container-fluid">
                             <div className={styles.compare}>
                                 <div className="row">
-                                    <div className="col-4">
+                                    <div className="col-12">
                                         <div className="table-responsive">
                                             <table className={`table table-bordered fixed ${styles.compare_table}`}>
                                                 <thead>
@@ -48,11 +49,106 @@ const page = () => {
                                                                 </div>
                                                             </div>
                                                         </th>
+                                                        <th>
+                                                            <div className={`${styles.product_box} ${styles.compare_product_box}`}>
+                                                                <div className={styles.product_timer}>
+                                                                    <div className={styles.timer_label}>
+                                                                        <span>تخفیف 40%</span>
+                                                                    </div>
+                                                                    <div className={styles.product_header_btn}>
+                                                                        <a href="" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="مقایسه">
+                                                                            <i className="bi bi-shuffle"></i>
+                                                                        </a>
+                                                                        <a href="" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="مقایسه">
+                                                                            <i className="bi bi-heart"></i>
+                                                                        </a>
+                                                                        <a href="" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="مقایسه">
+                                                                            <i className="bi bi-eye"></i>
+                                                                        </a>
+                                                                    </div>
+                                                                </div>
+                                                                <Link href={'/product/53535353'}>
+                                                                    <div className={styles.product_image}>
+                                                                        <img src="/images/product/watch2.jpg" alt="" className="img-fluid" />
+                                                                    </div>
+                                                                </Link>
+                                                                <div className={styles.product_title}>
+                                                                    <div className={styles.title}>
+                                                                        <p className="text-overflow-1">ساعت هوشمند شیائومی</p>
+                                                                        <span className='text-muted text-overflow-1'>Mibro Lite XPAW004 Smartwatch</span>
+                                                                    </div>
+                                                                    <div className={styles.rating}>
+                                                                        <div className={styles.number}>
+                                                                            <span className='text-muted font-12'>(15+) 4.8</span>
+                                                                        </div>
+                                                                        <div className={styles.icon}>
+                                                                            <i className="bi bi-star-fill"></i>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                            <div className={styles.compare_box}>
+                                                                <div className={styles.compare_delete}>
+                                                                    <a type='button' >
+                                                                        <i className='bi bi-x-circle-fill'></i>
+                                                                    </a>
+                                                                </div>
+                                                            </div>
+
+                                                        </th>
+                                                        <th>
+                                                            <div className={`${styles.product_box} ${styles.compare_product_box}`}>
+                                                                <div className={styles.product_timer}>
+                                                                    <div className={styles.timer_label}>
+                                                                        <span>تخفیف 40%</span>
+                                                                    </div>
+                                                                    <div className={styles.product_header_btn}>
+                                                                        <a href="" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="مقایسه">
+                                                                            <i className="bi bi-shuffle"></i>
+                                                                        </a>
+                                                                        <a href="" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="مقایسه">
+                                                                            <i className="bi bi-heart"></i>
+                                                                        </a>
+                                                                        <a href="" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="مقایسه">
+                                                                            <i className="bi bi-eye"></i>
+                                                                        </a>
+                                                                    </div>
+                                                                </div>
+                                                                <Link href={'/product/53535353'}>
+                                                                    <div className={styles.product_image}>
+                                                                        <img src="/images/product/watch2.jpg" alt="" className="img-fluid" />
+                                                                    </div>
+                                                                </Link>
+                                                                <div className={styles.product_title}>
+                                                                    <div className={styles.title}>
+                                                                        <p className="text-overflow-1">ساعت هوشمند شیائومی</p>
+                                                                        <span className='text-muted text-overflow-1'>Mibro Lite XPAW004 Smartwatch</span>
+                                                                    </div>
+                                                                    <div className={styles.rating}>
+                                                                        <div className={styles.number}>
+                                                                            <span className='text-muted font-12'>(15+) 4.8</span>
+                                                                        </div>
+                                                                        <div className={styles.icon}>
+                                                                            <i className="bi bi-star-fill"></i>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                            <div className={styles.compare_box}>
+                                                                <div className={styles.compare_delete}>
+                                                                    <a type='button' >
+                                                                        <i className='bi bi-x-circle-fill'></i>
+                                                                    </a>
+                                                                </div>
+                                                            </div>
+
+                                                        </th>
                                                     </tr>
+
                                                 </thead>
                                                 <tbody>
                                                     <tr>
-                                                        <td colSpan="5" className='text-end'>
+                                                        <td colSpan="5" className={styles.td_head}>
                                                             <i className="bi bi-chevron-double-left"></i>
                                                             <span>قیمت</span>
                                                         </td>
@@ -61,6 +157,83 @@ const page = () => {
                                                         <td colSpan="5" className={styles.td_head}>
                                                             <i className="bi bi-chevron-double-left"></i>
                                                             <span>پردازنده</span>
+                                                        </td>
+                                                    </tr>
+                                                    <tr >
+                                                        <td className='text-end pe-5'>
+                                                            مدل پردازنده گرافیکی
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td colSpan="5" className={styles.td_head}>
+                                                            <i className="bi bi-chevron-double-left"></i>
+                                                            <span>صفحه نمایش</span>
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td className='text-end pe-5'>
+                                                            ابعاد
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td className='text-end pe-5'>
+                                                            نوع صفحه نمایش
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td className='text-end pe-5'>
+                                                            رزولوشن
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td colSpan="5" className={styles.td_head}>
+                                                            <i className="bi bi-chevron-double-left"></i>
+                                                            <span>ارتباطات</span>
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td className='text-end pe-5'>
+                                                            شبکه هاي ارتباطي
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td className='text-end pe-5'>
+                                                            تعداد سیم کارت
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td colSpan="5" className={styles.td_head}>
+                                                            <i className="bi bi-chevron-double-left"></i>
+                                                            <span>پردازنده</span>
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td className='text-end pe-5'>
+                                                            مدل پردازنده گرافیکی
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td colSpan="5" className={styles.td_head}>
+                                                            <i className="bi bi-chevron-double-left"></i>
+                                                            <span>سیستم عامل</span>
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td colSpan="5" className={styles.td_head}>
+                                                            <i className="bi bi-chevron-double-left"></i>
+                                                            <span>حافظه Cache</span>
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td colSpan="5" className={styles.td_head}>
+                                                            <i className="bi bi-chevron-double-left"></i>
+                                                            <span> ظرفیت حافظه RAM</span>
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td colSpan="5" className={styles.td_head}>
+                                                            <i className="bi bi-chevron-double-left"></i>
+                                                            <span> قابلیت‌های دستگاه</span>
                                                         </td>
                                                     </tr>
                                                 </tbody>
