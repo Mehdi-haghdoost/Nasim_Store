111f5f8b482578d06ad66fd987115df90f9a9146
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2025-03-06 17:15:48 +0330

    Create Seller model: implement structure for managing sellers with product-specific prices

---
+++ models/Seller.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+
+const schema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+
+    address: {
+        type: String,
+        required: false,
+        trim: true,
+    },
+
+    contactNumber: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+
+    logo: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+
+    rating: {
+        type: Number,
+        default: 0,
+        min: 0,
+        max: 5,
+    },
+
+    product: [{
+        product: {
+            type: mongoose.Types.ObjectId,
+            ref: "Product",
+            required: true,
+        },
+        price: {
+            type: Number,
+            required: true,
+            min: 0,
+            default: 0
+        }
+    }],
+},
+    { timestamps: true }
+);
+
+const model = mongoose.models.Seller || mongoose.model("Seller", schema);
+
+module.exports = model;
