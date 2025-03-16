d18838b907903b61688a2cda3dde44248e8bf4f3
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2025-03-16 18:32:30 +0330

    Add category.resolvers.js: implement addCategory mutation with token validation and role check

--- src/graphql/index.resolver.js
+++ src/graphql/index.resolver.js
@@ -2,6 +2,7 @@ const { GraphQLObjectType, GraphQLSchema, GraphQLString } = require("graphql");
 const { registerUser,
     sendOtp,
     confirmOtpAndRegister, } = require("./mutations/user.resolvers");
+const { addProduct } = require("./mutations/product.resolvers");
 
 
 const RootQuery = new GraphQLObjectType({
@@ -22,6 +23,7 @@ const RootMutation = new GraphQLObjectType({
         registerUser,
         sendOtp,
         confirmOtpAndRegister,
+        addProduct
     },
 });
 
---
+++ src/graphql/mutations/category.resolvers.js
@@ -0,0 +1,40 @@
+const { GraphQLNonNull } = require("graphql");
+const CategoryModel = require("../../../models/Category");
+const { CategoryType, CategoryInputType } = require("../types/category.types");
+const { validateToken } = require("@/utils/auth");
+
+const addCategory = {
+    type: CategoryType,
+    args: {
+        input: { type: new GraphQLNonNull(CategoryInputType) },
+    },
+    resolve: async (_, { input }, context) => {
+        try {
+
+            const user = validateToken(context.req);
+            if (!user) {
+                throw new Error("احراز هویت نشده است")
+            }
+
+            if (user.role !== "ADMIN") {
+                throw new Error("دسترسی لازم برای انجام این کار را ندارید")
+            }
+
+            const newCategory = new CategoryModel({
+                ...input,
+                createdAt: new Date().toISOString(),
+                updatedAt: new Date().toISOString()
+            });
+
+            const savedCategory = await newCategory.save();
+            return savedCategory;
+
+        } catch (error) {
+            throw new Error(`خطا در ایجاد دسته بندی ${error.message}`)
+        }
+    }
+};
+
+module.exports = {
+    addCategory,
+};
