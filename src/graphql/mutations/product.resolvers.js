68822080e7bcbfa16bef29c543bc217a11e5e436
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2025-03-17 22:19:29 +0330

    Update addProduct: add validations for discountedPrice

--- src/graphql/mutations/product.resolvers.js
+++ src/graphql/mutations/product.resolvers.js
@@ -13,6 +13,9 @@ const addProduct = {
     resolve: async (_, { input }, context) => {
         try {
             const { role } = await validateToken(context.req);
+            if (!role) {
+                throw new Error("احراز هویت لازم می باشد")
+            }
             if (role !== "ADMIN") {
                 throw new Error("دسترسی لازم برای انجام این کار را ندارید")
             }
@@ -31,11 +34,47 @@ const addProduct = {
                 }
             }
 
+            // اعتبار سنجی برای کد تخفیف
+            if (input.hasDiscount) {
+                if (!input.discountedPrice || input.discountedPrice <= 0) {
+                    throw new Error("قیمت تخفیف خورده را وارد کنید وبیشتراز صفر باشد")
+                }
+
+                if (input.discountedPrice >= input.price) {
+                    throw new Error("قیمت تخفیف خورده باید کمتراز قیمت اصلی باشد")
+                }
+            } else {
+                input.discountedPrice = null;
+            }
+
             const newProduct = new ProductModel({
-                ...input,
+                image: input.image,
+                title: input.title,
+                features: input.features || [],
+                rating: input.rating || 0,
+                highlights: input.highlights || [],
+                category: input.category,
+                colors: input.colors || [
+                    { color: "قرمز", available: true },
+                    { color: "مشکی", available: true },
+                    { color: "آبی", available: true },
+                    { color: "سبز", available: false },
+                ],
+                stock: input.stock || 0,
+                price: input.price,
+                discountedPrice: input.discountedPrice,
+                hasDiscount: input.hasDiscount || false,
+                brandIcon: input.brandIcon,
+                originalName: input.originalName,
+                description: input.description,
+                specifications: input.specifications,
+                comments: input.comments || [],
+                sellers: input.sellers,
+                customerSatisfaction: input.customerSatisfaction || 0,
+                salesCount: input.salesCount || 0,
                 createdAt: new Date().toISOString(),
                 updatedAt: new Date().toISOString(),
-            })
+            });
 
             const savedProduct = await newProduct.save();
 
