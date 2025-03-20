8c8974c793ad57fe73cbb83b2cba80a7ec296922
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2025-03-20 11:55:19 +0330

    Update CategoryType with resolvers for referenced fields to fix null errors

--- src/graphql/types/category.types.js
+++ src/graphql/types/category.types.js
@@ -1,14 +1,29 @@
-const { GraphQLObjectType, GraphQLNonNull, GraphQLString, GraphQLID, GraphQLInputObjectType } = require("graphql");
+const { GraphQLObjectType, GraphQLNonNull, GraphQLString, GraphQLID, GraphQLInputObjectType, GraphQLList } = require("graphql");
 
 const CategoryType = new GraphQLObjectType({
     name: "CategoryType",
-    fields: () => ({
-        _id: { type: new GraphQLNonNull(GraphQLID) },
-        name: { type: new GraphQLNonNull(GraphQLString) },
-        icon: { type: new GraphQLNonNull(GraphQLString) },
-        createdAt: { type: new GraphQLNonNull(GraphQLString) },
-        updatedAt: { type: new GraphQLNonNull(GraphQLString) },
-    })
+    fields: () => {
+        const { ProductType } = require("./product.types");
+        const ProductModel = require("../../../models/Product");
+        return {
+            _id: { type: new GraphQLNonNull(GraphQLID) },
+            name: { type: new GraphQLNonNull(GraphQLString) },
+            icon: { type: new GraphQLNonNull(GraphQLString) },
+            createdAt: { type: new GraphQLNonNull(GraphQLString) },
+            updatedAt: { type: new GraphQLNonNull(GraphQLString) },
+            products: {
+                type: new GraphQLList(ProductType),
+                resolve: async (parent) => {
+                    try {
+                        return await ProductModel.find({ category: parent._id });
+                    } catch (error) {
+                        throw new Error(`خطا در گرفتن دسته بندی محصولات ${error.message}`)
+                    }
+                }
+            }
+        }
+
+    }
 });
 
 const CategoryInputType = new GraphQLInputObjectType({
