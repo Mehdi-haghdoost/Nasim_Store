40add167a2e30d1802e2b91f5998ef7a3bc01344
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2025-03-20 11:55:19 +0330

    Add resolvers for category, sellers, and comments in ProductType to fix null field errors

--- src/graphql/types/product.types.js
+++ src/graphql/types/product.types.js
@@ -1,7 +1,7 @@
 const { GraphQLObjectType, GraphQLID, GraphQLNonNull, GraphQLString, GraphQLList, GraphQLFloat, GraphQLBoolean, GraphQLInt, GraphQLInputObjectType } = require("graphql");
-const {CategoryType} = require("./category.types");
-const {SellerType} = require("./seller.types");
-const {CommentType} = require("./comment.types");
+const { CategoryType } = require("./category.types");
+const { SellerType } = require("./seller.types");
+const { CommentType } = require("./comment.types");
 
 const HighlightType = new GraphQLObjectType({
     name: "HighlightType",
@@ -18,46 +18,109 @@ const ColorType = new GraphQLObjectType({
     })
 })
 
+const FeatureType = new GraphQLObjectType({
+    name: "FeatureType",
+    fields: () => ({
+        key: { type: new GraphQLNonNull(GraphQLString) },
+        value: { type: new GraphQLNonNull(GraphQLString) },
+    })
+})
+
+const FeatureInputType = new GraphQLInputObjectType({
+    name: "FeatureInput",
+    fields: () => ({
+        key: { type: new GraphQLNonNull(GraphQLString) },
+        value: { type: new GraphQLNonNull(GraphQLString) },
+    }),
+})
+
 const ProductType = new GraphQLObjectType({
     name: "ProductType",
-    fields: {
-        _id: { type: new GraphQLNonNull(GraphQLID) },
-        image: { type: new GraphQLNonNull(GraphQLString) },
-        title: { type: new GraphQLNonNull(GraphQLString) },
-        features: { type: new GraphQLNonNull(new GraphQLList(GraphQLString)) },
-        rating: { type: GraphQLFloat },
-        highlights: { type: new GraphQLNonNull(new GraphQLList(HighlightType)) },
-        category: { type: new GraphQLNonNull(CategoryType) },
-        color: { type: new GraphQLNonNull(new GraphQLList(ColorType)) },
-        stock: { type: new GraphQLNonNull(GraphQLInt) },
-        price: { type: new GraphQLNonNull(GraphQLFloat) },
-        discountedPrice: { type: GraphQLFloat },
-        hasDiscount: { type: GraphQLBoolean },
-        brandIcon: { type: new GraphQLNonNull(GraphQLString) },
-        originalName: { type: new GraphQLNonNull(GraphQLString) },
-        description: { type: new GraphQLNonNull(GraphQLString) },
-        specifications: { type: new GraphQLNonNull(GraphQLString) },
-        comments: { type: new GraphQLList(CommentType) },
-        sellers: { type: new GraphQLNonNull(new GraphQLList(SellerType)) },
-        customerSatisfaction: { type: GraphQLFloat },
-        salesCount: { type: new GraphQLNonNull(GraphQLInt) },
-        createdAt: { type: new GraphQLNonNull(GraphQLString) },
-        updatedAt: { type: new GraphQLNonNull(GraphQLString) },
+    fields: () => {
+
+        const CategoryModel = require("../../../models/Category");
+        const SellerModel = require("../../../models/Seller");
+        const CommentModel = require("../../../models/Comment");
+        return {
+            _id: { type: new GraphQLNonNull(GraphQLID) },
+            image: { type: new GraphQLNonNull(GraphQLString) },
+            title: { type: new GraphQLNonNull(GraphQLString) },
+            features: { type: new GraphQLList(FeatureType) },
+            rating: { type: GraphQLFloat },
+            highlights: { type: new GraphQLNonNull(new GraphQLList(HighlightType)) },
+            category: {
+                type: new GraphQLNonNull(CategoryType),
+                resolve: async (parent) => {
+                    try {
+                        const category = await CategoryModel.findById(parent.category);
+                        if (!category) {
+                            console.log(`دسته بندی با شناسه ${parent.category} یافت نشد`);
+                            throw new Error(`دسته‌بندی یافت نشد`)
+                        }
+                        return category;
+                    } catch (error) {
+                        console.error(`خطا در بازیابی دسته‌بندی:`, error);
+                        throw new Error(`خطا در بازیابی دسته‌بندی: ${error.message}`);
+                    }
+                }
+            },
+            colors: { type: new GraphQLNonNull(new GraphQLList(ColorType)) },
+            stock: { type: new GraphQLNonNull(GraphQLInt) },
+            price: { type: new GraphQLNonNull(GraphQLFloat) },
+            discountedPrice: { type: GraphQLFloat },
+            hasDiscount: { type: GraphQLBoolean },
+            brandIcon: { type: new GraphQLNonNull(GraphQLString) },
+            originalName: { type: new GraphQLNonNull(GraphQLString) },
+            description: { type: new GraphQLNonNull(GraphQLString) },
+            specifications: { type: new GraphQLNonNull(GraphQLString) },
+            comments: {
+                type: new GraphQLList(CommentType),
+                resolve: async (parent) => {
+                    try {
+                        if (!parent.comment || parent.comment.length === 0) {
+                            return [];
+                        }
+
+                        const comments = await CommentModel.find({ _id: { $in: parent.comments } });
+                        return comments;
+                    } catch (error) {
+                        console.log(`خطا در بازیبای کامنت ها ${error.message}`);
+                        throw new Error(`خطا در بازیبای کامنت ها ${error.message}`);
+                    }
+                }
+            },
+            sellers: { 
+                type: new GraphQLNonNull(new GraphQLList(SellerType)),
+                resolve : async (parent) => {
+                    try {
+                        const sellers = await SellerModel.find({ _id: { $in: parent.sellers } });
+                        return sellers;
+                    } catch (error) {
+                        console.error(`خطا در بازیابی فروشندگان:`, error);
+                        throw new Error(`خطا در بازیابی فروشندگان: ${error.message}`);
+                    }
+                }
+             },
+            customerSatisfaction: { type: GraphQLFloat },
+            salesCount: { type: new GraphQLNonNull(GraphQLInt) },
+            createdAt: { type: new GraphQLNonNull(GraphQLString) },
+            updatedAt: { type: new GraphQLNonNull(GraphQLString) },
+        };
     }
 })
 
 const HighlightInputType = new GraphQLInputObjectType({
-    name : "HighlightInput",
-    fields : () => ({
-        feature : {type : new GraphQLNonNull(GraphQLString)},
+    name: "HighlightInput",
+    fields: () => ({
+        feature: { type: new GraphQLNonNull(GraphQLString) },
     }),
 });
 
 const ColorInputType = new GraphQLInputObjectType({
-    name : "ColorInput",
-    fields : () => ({
-        color : {type : new GraphQLNonNull(GraphQLString)},
-        available : {type : GraphQLBoolean},
+    name: "ColorInput",
+    fields: () => ({
+        color: { type: new GraphQLNonNull(GraphQLString) },
+        available: { type: GraphQLBoolean },
     }),
 })
 
@@ -66,7 +129,7 @@ const ProductInputType = new GraphQLInputObjectType({
     fields: () => ({
         image: { type: new GraphQLNonNull(GraphQLString) },
         title: { type: new GraphQLNonNull(GraphQLString) },
-        features: { type: new GraphQLList(GraphQLString) },
+        features: { type: new GraphQLList(FeatureInputType) },
         rating: { type: GraphQLFloat },
         highlights: { type: new GraphQLList(HighlightInputType) },
         category: { type: new GraphQLNonNull(GraphQLID) },
@@ -92,5 +155,7 @@ module.exports = {
     HighlightType,
     ColorType,
     HighlightInputType,
-    ColorInputType
+    ColorInputType,
+    FeatureType,
+    FeatureInputType,
 }
