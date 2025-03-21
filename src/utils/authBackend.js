00fdb4992c22b195503c97c8f1b568337f676656
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2025-03-21 08:41:00 +0330

    Add setAuthCookies function to authBackend.js
    Moved the setAuthCookies helper function from user.resolvers.js to authBackend.js to centralize authentication-related logic.

--- src/utils/authBackend.js
+++ src/utils/authBackend.js
@@ -1,6 +1,21 @@
 const jwt = require("jsonwebtoken");
 const UserModel = require("../../models/User");
 
+// تابع کمکی برای ست کردن کوکی‌ها
+const setAuthCookies = (res, accessToken, refreshToken) => {
+    res.cookie('accessToken', accessToken, {
+        httpOnly: true, // کوکی فقط از طریق سرور قابل دسترسیه
+        maxAge: 1 * 60 * 60 * 1000,
+        sameSite: 'Strict', // جلوگیری از CSRF
+    });
+
+    res.cookie('refreshToken', refreshToken, {
+        httpOnly: true,
+        maxAge: 30 * 24 * 60 * 60 * 1000, // ۳۰ روز (هماهنگ با Refresh Token)
+        sameSite: 'Strict',
+    });
+}
+
 const getUserById = (id) => {
     return UserModel.findOne({ _id: id });
 }
@@ -19,7 +34,7 @@ const validateToken = async (req) => {
             if (!AccessTokenSecretKey) {
                 throw new Error("کلید مخفی توکن در فایل .env تنظیم نشده است");
             }
-            
+
             const { id } = jwt.verify(token, AccessTokenSecretKey);
             const user = await getUserById(id);
 
@@ -38,4 +53,5 @@ const validateToken = async (req) => {
 
 module.exports = {
     validateToken,
+    setAuthCookies,
 }
