4b19f9a3a44497e1f3776c03e73a620a5ac52607
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2025-03-29 20:39:28 +0330

    Add getUserCart resolver to fetch the user's cart with products

---
+++ src/graphql/queries/cart.resolvers.js
@@ -0,0 +1,56 @@
+const { validateToken } = require("../../utils/authBackend");
+const { CartType } = require("../types/cart.types");
+const UserModel = require('../../../models/User');
+
+const getUserCart = {
+    type: CartType,
+    resolve: async (_, args, { req }) => {
+        try {
+            const user = await validateToken(req);
+            if (!user) {
+                throw new Error('کاربر احراز هویت نشده است');
+            }
+
+            const populatedUser = await UserModel.findById(user._id)
+                .populate({
+                    path: 'cart.product',
+                    model: 'product'
+                })
+                .populate({
+                    path: 'cart.selectedSeller',
+                    model: 'Seller'
+                })
+            // محاسبه قیمت کل، تخفیف و قیمت نهایی
+            let totalPrice = 0;
+            let totalDiscount = 0;
+
+            populatedUser.cart.forEach(item => {
+                const product = item.product;
+                const quantity = item.quantity;
+
+                if (product.hasDiscount) {
+                    totalPrice += product.price * quantity;
+                    totalDiscount += (product.price - product.discountedPrice) * quantity;
+                } else {
+                    totalPrice += product.price * quantity;
+                }
+            });
+
+            const finalPrice = totalPrice - totalDiscount;
+
+            return {
+                items: populatedUser.cart,
+                totalPrice,
+                totalDiscount,
+                finalPrice,
+            };
+
+        } catch (error) {
+            throw new Error(`خطا در دریافت سبد خرید: ${error.message}`);
+        }
+    }
+}
+
+module.exports = {
+    getUserCart
+};
