2de23d04af4994af4c6e7f1e24fe9b9b8a690038
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2025-03-29 20:38:22 +0330

    Define CartItemType for cart items with product, quantity, color, and size

---
+++ src/graphql/types/cart.types.js
@@ -0,0 +1,56 @@
+const { GraphQLObjectType, GraphQLID, GraphQLInt, GraphQLString, GraphQLList, GraphQLFloat, GraphQLInputObjectType, GraphQLNonNull } = require("graphql");
+const { SellerType } = require("./seller.types");
+
+// تعریف نوع آیتم سبد خرید
+const CartItemType = new GraphQLObjectType({
+    name: "CartItem",
+    fields: {
+        _id: { type: GraphQLID },
+        product: { type: ProductType },
+        quantity: { type: GraphQLInt },
+        color: { type: GraphQLString },
+        siza: { type: GraphQLString },
+        selectedSeller: { type: SellerType },
+    }
+});
+
+// تعریف نوع سبد خرید
+
+const CartType = new GraphQLObjectType({
+    name: "Cart",
+    fields: {
+        items: { type: new GraphQLList(CartItemType) },
+        totalPrice: { type: GraphQLFloat },
+        totalDiscount: { type: GraphQLFloat },
+        finalPrice: { type: GraphQLFloat },
+    }
+});
+
+// ورودی برای افزودن محصول به سبد خرید
+const AddToCartInputType = new GraphQLInputObjectType({
+    name: "AddToCartInput",
+    fields: {
+        productId: { type: new GraphQLNonNull(GraphQLID) },
+        quantity: { type: GraphQLInt },
+        color: { type: GraphQLString },
+        size: { type: GraphQLString },
+        sellerId: { type: GraphQLID },
+    }
+});
+
+// ورودی برای بروزرسانی محصول در سبد خرید
+const UpdateCartItemInputType = new GraphQLInputObjectType({
+    name: "UpdateCartItemInput",
+    fields: {
+        itemId: { type: new GraphQLNonNull(GraphQLID) },
+        quantity: { type: new GraphQLNonNull(GraphQLInt) },
+    }
+});
+
+
+module.exports = {
+    CartItemType,
+    CartType,
+    AddToCartInputType,
+    UpdateCartItemInputType
+};
