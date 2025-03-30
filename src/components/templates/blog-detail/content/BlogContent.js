0893da1f30e2252a5707cedb34aeb8b11e90c484
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2025-03-30 21:28:49 +0330

    Update BlogContent to receive and pass MDX data to BlogDetail

--- src/components/templates/blog-detail/content/BlogContent.js
+++ src/components/templates/blog-detail/content/BlogContent.js
@@ -1,24 +1,23 @@
-import React from 'react'
-import sstyles from './BlogContent.module.css';
+import React from 'react';
+import styles from './BlogContent.module.css';
 import BlogDetail from './BlogDetail';
 import SideBlogDetail from './SideBlogDetail';
 
-
-const BlogContent = () => {
-    return (
-        <div className='content'>
-            <div className="container-fluid">
-                <div className="row gy-3">
-                    <div className="col-lg-9">
-                        <BlogDetail />
-                    </div>
-                    <div className="col-lg-3">
-                        <SideBlogDetail />
-                    </div>
-                </div>
-            </div>
+const BlogContent = ({ postData }) => {
+  return (
+    <div className='content'>
+      <div className="container-fluid">
+        <div className="row gy-3">
+          <div className="col-lg-9">
+            <BlogDetail postData={postData} />
+          </div>
+          <div className="col-lg-3">
+            <SideBlogDetail />
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default BlogContent
+export default BlogContent;
