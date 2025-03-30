223ad6fe273241655ab4432849ac84ab476dd505
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2025-03-30 21:27:22 +0330

    Update blog detail page to fetch content from MDX files

--- src/app/blog-detail/[id]/page.js
+++ src/app/blog-detail/[id]/page.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import styles from '@/styles/blog-detail.module.css';
 import Header from '@/components/modules/header/Header';
 import BreadCroumb from '@/components/modules/breadCroumb/BreadCroumb';
@@ -7,19 +7,33 @@ import BlogContent from '@/components/templates/blog-detail/content/BlogContent'
 import BlogComments from '@/components/templates/blog-detail/blog-comments/BlogComments';
 import CommentForm from '@/components/templates/blog-detail/FeedbackForm/CommentForm';
 import RelatedArticles from '@/components/templates/blog-detail/RelatedArticles/RelatedArticles';
+import { getAllPostIds, getPostData, getAllPosts } from '@/lib/mdx';
 
-const page = () => {
-    return (
-        <>
-            <Header />
-            <BreadCroumb />
-            <BlogContent />
-            <BlogComments />
-            <CommentForm />
-            <RelatedArticles />
-            <Footer />
-        </>
-    )
+// تولید مسیرهای استاتیک برای همه پست‌های بلاگ
+export async function generateStaticParams() {
+  const paths = getAllPostIds();
+  return paths.map(path => path.params);
 }
 
-export default page
+export default async function BlogDetailPage({ params }) {
+  const { id } = params;
+  const postData = getPostData(id);
+  
+  // دریافت پست‌های مرتبط (با دسته‌بندی یکسان)
+  const allPosts = getAllPosts();
+  const relatedPosts = allPosts
+    .filter(post => post.category === postData.category && post.id !== id)
+    .slice(0, 8);
+  
+  return (
+    <>
+      <Header />
+      <BreadCroumb />
+      <BlogContent postData={postData} />
+      <BlogComments />
+      <CommentForm />
+      <RelatedArticles posts={relatedPosts} />
+      <Footer />
+    </>
+  );
+}
