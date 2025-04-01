b446b76e1dd73b886d5607da1d54fef8a66901cc
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2025-04-01 14:45:46 +0330

    Update CategoryBlogsItem to add random post selection for empty items

--- src/components/templates/CategoryBlogs/content/CategoryBlogsItem.js
+++ src/components/templates/CategoryBlogs/content/CategoryBlogsItem.js
@@ -1,40 +1,3 @@
-// import React from 'react'
-// import styles from './CategoryBlogsItem.module.css';
-// import Link from 'next/link';
-
-// const CategoryBlogsItem = () => {
-//     return (
-        
-//             <div className="col-md-3">
-//                 <div className={styles.blog_item}>
-//                     <Link href={"/blog-detail/2123"}>
-//                         <div className={styles.image}>
-//                             <img src="/images/blog-1.jpg" className='img-fluid' alt="blogimage" />
-//                         </div>
-//                         <div className={styles.title}>
-//                             <div className='d-flex align-items-center justify-content-between'>
-//                                 <div className={styles.title_item}>
-//                                     <i className="bi bi-tag ms-1"></i>
-//                                     <span className='font-12'>آموزشی</span>
-//                                 </div>
-//                                 <div className={styles.title_item}>
-//                                     <i className="bi bi-alarm ms-1"></i>
-//                                     <span className='font-12'>16 آذر 1403</span>
-//                                 </div>
-//                             </div>
-//                             <h4 className="font-16 text-overflow-1 h4">
-//                                 ارز دیجیتال چیست و چگونه استخراج میشود؟
-//                             </h4>
-//                         </div>
-//                     </Link>
-//                 </div>
-//             </div>
-        
-//     )
-// }
-
-// export default CategoryBlogsItem
-
 import React from 'react';
 import styles from './CategoryBlogsItem.module.css';
 import Link from 'next/link';
@@ -45,7 +8,7 @@ const CategoryBlogsItem = ({ post }) => {
         return (
             <div className="col-md-3">
                 <div className={styles.blog_item}>
-                    <Link href={"/blog-detail/2123"}>
+                    <Link href={`/blog-detail/${post?.id}`}>
                         <div className={styles.image}>
                             <img src="/images/blog-1.jpg" className='img-fluid' alt="blogimage" />
                         </div>
