02caf4ddc7fd2172c85150c8c510e52725ecb5bd
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2025-04-01 14:46:59 +0330

    Update CategoryBlogsPosts component for better post rendering

--- src/components/templates/CategoryBlogs/content/CategoryBlogsPosts.js
+++ src/components/templates/CategoryBlogs/content/CategoryBlogsPosts.js
@@ -1,26 +1,3 @@
-// import React from 'react'
-// import styles from './CategoryBlogsPosts.module.css';
-// import CategoryBlogsItem from './CategoryBlogsItem';
-
-// const CategoryBlogsPosts = () => {
-//     return (
-//         <div className='row gy-3'>
-//             <CategoryBlogsItem />
-//             <CategoryBlogsItem />
-//             <CategoryBlogsItem />
-//             <CategoryBlogsItem />
-//             <CategoryBlogsItem />
-//             <CategoryBlogsItem />
-//             <CategoryBlogsItem />
-//             <CategoryBlogsItem />
-//         </div>
-
-
-//     )
-// }
-
-// export default CategoryBlogsPosts
-
 "use client";
 
 import React from 'react';
