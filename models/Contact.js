6e18e08bc74b2245ed59f50ebe3000f3188f3724
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2025-04-03 19:02:56 +0330

    Contact model schema for storing contact form submissions

---
+++ models/Contact.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "نام الزامی است"],
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: [true, "ایمیل الزامی است"],
+        match: [/[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/g, "لطفا یک ایمیل معتبر وارد کنید"],
+        trim: true,
+    },
+    messgae: {
+        type: String,
+        trim: true,
+        default: "",
+    },
+    saveInfo: {
+        type: Boolean,
+        default: false,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const model = mongoose.models.Contact || mongoose.model("Contact", schema);
+export default model;
