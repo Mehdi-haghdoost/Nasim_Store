03411a35fbb1338dea5b076fbe74e87949556fa6
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2025-04-05 10:25:15 +0330

    Apollo Client Configuration  * Sets up the Apollo Client with proper caching and link configuration  * The URI should point to your GraphQL endpoint (adjust as needed)

---
+++ src/graphql/client/index.js
@@ -0,0 +1,23 @@
+const { HttpLink, ApolloClient, InMemoryCache } = require("@apollo/client");
+
+const httpLink = new HttpLink({
+    uri: "http://localhost:4005/graphql",
+});
+
+const client = new ApolloClient({
+    link: createLinks(httpLink),
+    cache: new InMemoryCache(),
+    defaultOptions: {
+        fetchPolicy: 'cache-and-network',
+        errorPolicy: 'ignore',
+    },
+    query: {
+        fetchPolicy: 'network-only',
+        errorPolicy: 'all',
+    },
+    mutate: {
+        errorPolicy: 'all',
+    },
+});
+
+export default client;
