a8de820b0ee216eb84ca3651e8ce470ccc385fad
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2025-04-07 16:56:38 +0330

    feat(redux): Add middleware configuration with Redux Thunk support

--- src/Redux/store/middleware.js
+++ src/Redux/store/middleware.js
@@ -1,1 +1,13 @@
 // # تنظیمات میدل‌ورها
+import { thunk } from "redux-thunk";
+/**
+ * Custom Redux middleware setup
+ * Configure middleware for logging, async actions, etc.
+ */
+
+export const setupMiddleware = (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(
+        thunk,
+    );
+};
+
