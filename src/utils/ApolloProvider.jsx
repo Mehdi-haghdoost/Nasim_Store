2fb472fb45ce9df511e46510d74baeca2d5c9d6b
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2025-04-07 16:12:10 +0330

    feat(providers): Create unified ApolloProvider with Redux integration

--- src/utils/ApolloProvider.jsx
+++ src/utils/ApolloProvider.jsx
@@ -1,8 +1,22 @@
 "use client";
 
 import { ApolloProvider as Provider } from "@apollo/client";
+import { provider as ReduxProvider } from 'react-redux';
 import client from "@/graphql/client";
+import store from '@/store';
+
+/**
+ * Providers component
+ * Wraps the application with necessary providers (Redux and Apollo)
+ */
+
 
 export default function ApolloProvider({ children }) {
-    return <Provider client={client} >{children}</Provider>
+    return (
+        <ReduxProvider store={store}>
+            <Provider client={client}>
+                {children}
+            </Provider>
+        </ReduxProvider>
+    );
 };
