823f5d1fcffdaaf8bc4851cbf622d5764790ce4d
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2025-04-07 16:28:24 +0330

    feat(redux): Create root reducer file to combine all reducers

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,1 +1,15 @@
 // # ترکیب کننده تمام ریدیوسرها
+
+import { combineReducers } from "redux";
+import authReducer from './slices/authSlice';
+
+
+/**
+ * Root reducer combining all slice reducers
+ * Add new reducers here as the application grows
+ */
+
+export const rootReducer = combineReducers({
+    auth: authReducer,
+    // اضافه کردن سایر ریدیوسرها در اینجا
+});
