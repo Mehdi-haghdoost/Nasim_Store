f3c7af02292dc58ebe12f229f986a58d262e9ff5
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2025-04-12 13:57:31 +0330

    Rename name field to username in register validation schema

--- src/utils/register.js
+++ src/utils/register.js
@@ -1,7 +1,7 @@
 import * as Yup from "yup";
 
 const registerSchema = Yup.object().shape({
-    name: Yup.string()
+    username: Yup.string()
         .min(3, "نام حداقل باید 3 کاراکتر داشته باشد")
         .max(12, "نام حداکثر باید 12 کاراکتر داشته باشد")
         .required('ورودی مورد نیاز است'),
