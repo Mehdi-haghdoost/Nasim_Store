a139b05e7ec5cd2372e40316a5590494b7c1d658
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2025-04-12 13:34:42 +0330

    Remove authLink to rely on cookie-based authentication in Apollo Client

--- src/graphql/client/links.js
+++ src/graphql/client/links.js
@@ -12,19 +12,7 @@ const errorLink = onError(({ graphQLErrors, networkError }) => {
     if (networkError) console.error(`[Network error]: ${networkError}`);
 });
 
-const authLink = setContext((_, { headers }) => {
-    // Get the authentication token from local storage if it exists
-    const token = localStorage.getItem('token');
-
-    // Return the headers to the context so httpLink can read them
-    return {
-        headers: {
-            ...headers,
-            authorization: token ? `Bearer ${token}` : '',
-        }
-    };
-});
 
 export const createLinks = (httpLink) => {
-    return from([errorLink, authLink, httpLink]);
+    return from([errorLink, httpLink]);
 };
