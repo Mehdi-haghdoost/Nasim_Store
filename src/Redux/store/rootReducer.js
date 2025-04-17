c3948476dc0002505135399a3bdf2701b319c5b5
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2025-04-17 18:27:13 +0330

    Fix file naming and update root reducer with new slices

--- src/Redux/store/rootReducer.js
+++ src/Redux/store/rootReducer.js
@@ -1,17 +1,16 @@
-// # ترکیب کننده تمام ریدیوسرها
-
 import { combineReducers } from "redux";
 import authReducer from '../slices/authSlice';
 import cartReducer from '../slices/cartSlice';
-
+import productReducer from '../slices/productSlice';
+import toastReducer from '../slices/toastSlice';
 
 /**
  * Root reducer combining all slice reducers
  * Add new reducers here as the application grows
  */
-
 export const rootReducer = combineReducers({
     auth: authReducer,
     cart: cartReducer,
-    // اضافه کردن سایر ریدیوسرها در اینجا
+    product: productReducer,
+    toast: toastReducer,
 });
