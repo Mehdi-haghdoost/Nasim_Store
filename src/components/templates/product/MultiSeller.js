52dd9bce1cd85be289dbfc01aadd4f358027ea9b
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2025-04-19 18:31:55 +0330

    Refine MultiSeller component for better integration with product page updates

--- src/components/templates/product/MultiSeller.js
+++ src/components/templates/product/MultiSeller.js
@@ -9,13 +9,13 @@ function MultiSeller({ product, sellers }) {
 
     // استفاده از state برای مدیریت هیدراسیون
     const [isClient, setIsClient] = useState(false);
-    
+
     // انتخاب رنگ پیش‌فرض از محصول
     const [defaultColor, setDefaultColor] = useState(null);
 
     useEffect(() => {
         setIsClient(true);
-        
+
         // پیدا کردن رنگ پیش‌فرض از محصول
         if (product && product.colors && product.colors.length > 0) {
             // ابتدا تلاش می‌کنیم اولین رنگ موجود را پیدا کنیم
@@ -145,7 +145,7 @@ function MultiSeller({ product, sellers }) {
                                                         d="M8.186 1.113a.5.5 0 0 0-.372 0L1.846 3.5l2.404.961L10.404 2l-2.218-.887zm3.564 1.426L5.596 5 8 5.961 14.154 3.5l-2.404-.961zm3.25 1.7-6.5 2.6v7.922l6.5-2.6V4.24zM7.5 14.762V6.838L1 4.239v7.923l6.5 2.6zM7.443.184a1.5 1.5 0 0 1 1.114 0l7.129 2.852A.5.5 0 0 1 16 3.5v8.662a1 1 0 0 1-.629.928l-7.185 2.874a.5.5 0 0 1-.372 0L.63 13.09a1 1 0 0 1-.63-.928V3.5a.5.5 0 0 1 .314-.464L7.443.184z">
                                                     </path>
                                                 </svg>
-                                                <p className='d-inline me-2 mb-0 font-14'>{seller.shippingMethod || 'نامشخص'}</p>
+                                                <p className='d-inline me-2 mb-0 font-14'>{`ارسالی از ${seller.name}`}</p>
                                             </td>
                                             <td>
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor"
