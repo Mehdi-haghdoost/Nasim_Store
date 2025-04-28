21eb963fdedb8ea05bef5cc41f735a066af1997d
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2025-04-28 10:33:43 +0330

    Update useProduct hook to handle fetching products list

--- src/Redux/hooks/useProduct.js
+++ src/Redux/hooks/useProduct.js
@@ -1,6 +1,8 @@
+// src/Redux/hooks/useProduct.js
 import { useSelector, useDispatch } from "react-redux";
-import { fetchProduct } from "../actions/productThunks";
-import { clearError, clearProduct } from "../slices/productSlice";
+import { fetchProduct, fetchProducts } from "../actions/productThunks";
+import { clearError, clearProduct, clearProducts } from "../slices/productSlice";
+import { useEffect } from "react";
 
 export const useProduct = () => {
     const dispatch = useDispatch();
@@ -16,13 +18,33 @@ export const useProduct = () => {
         return dispatch(clearProduct());
     };
 
+    const getProducts = () => {
+        return dispatch(fetchProducts());
+    };
+
+    const resetProducts = () => {
+        return dispatch(clearProducts());
+    };
+
+    // لود خودکار محصولات هنگام اولین استفاده از هوک (اختیاری)
+    useEffect(() => {
+        if (productState.products.length === 0 && !productState.productsLoading && !productState.productsError) {
+            getProducts();
+        }
+    }, []);
+
     return {
         product: productState.product,
+        products: productState.products,
         loading: productState.loading,
+        productsLoading: productState.productsLoading,
         error: productState.error,
+        productsError: productState.productsError,
         getProduct,
+        getProducts,
         clearError: () => dispatch(clearError()),
         resetProduct,
+        resetProducts,
     };
 };
 
