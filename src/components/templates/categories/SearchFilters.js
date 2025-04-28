36c948ae78b2cfcbe092ef17a30eaeea0697ee1c
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2025-04-28 10:24:24 +0330

    Update SearchFilters component to use dynamic categories while preserving all UI elements

--- src/components/templates/categories/SearchFilters.js
+++ src/components/templates/categories/SearchFilters.js
@@ -29,7 +29,21 @@ const SearchFilters = () => {
     return (
         <div className={`${styles.filter_items} position-sticky top-0`}>
             <div className="container-fluid">
-                {/* ... بقیه کد بدون تغییر ... */}
+                <div className={styles.filter_item}>
+                    <h5 className={styles.filter_item_title}>جستجو</h5>
+                    <div className={styles.filter_item_content}>
+                        <div className={styles.search_form}>
+                            <form action="">
+                                <div className={styles.search_field}>
+                                    <input type="text" placeholder='جستجوی محصولات ...' className={`form-control ${styles.search_input}`} />
+                                    <button className={`btn main-color-one-bg rounded-pill ${styles.search_btn}`} type='submit'>
+                                        <i className="bi bi-search"></i>
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
                 <div className={styles.filter_item}>
                     <h5 className={styles.filter_item_title}>دسته بندی ها</h5>
                     <div className={styles.filter_item_content}>
@@ -65,10 +79,71 @@ const SearchFilters = () => {
                         </form>
                     </div>
                 </div>
-                {/* ... بقیه کد بدون تغییر ... */}
+                <div className={styles.filter_item}>
+                    <PriceRangeSlider />
+                </div>
+                <div className={styles.filter_item}>
+                    <h5 className={styles.filter_item_title}>رنگ محصول</h5>
+                    <div className={styles.filter_item_content}>
+                        <div className='product-meta-color-items'>
+                            <input type="radio" className='btn-check' name='options' id='option11'
+                                autoComplete='off'
+                            />
+                            <label htmlFor="option11" className='btn'>
+                                <span style={{ backgroundColor: "#c00" }}></span>
+                                قرمز
+                            </label>
+
+                            <input type="radio" className='btn-check' name='options' id='option22'
+                                autoComplete='off' defaultChecked
+                            />
+                            <label htmlFor="option22" className='btn'>
+                                <span style={{ backgroundColor: "#111" }}></span>
+                                مشکی
+                            </label>
+
+                            <input type="radio" className='btn-check' name='options' id='option33'
+                                autoComplete='off'
+                            />
+                            <label htmlFor="option33" className='btn'>
+                                <span style={{ backgroundColor: "#00cc5f" }}></span>
+                                سبز
+                            </label>
+
+                            <input type="radio" className='btn-check' name='options' id='option44'
+                                autoComplete='off'
+                            />
+                            <label htmlFor="option44" className='btn'>
+                                <span style={{ backgroundColor: "#1b69f0" }}></span>
+                                آبی
+                            </label>
+
+                            <input type="radio" className='btn-check' name='options' id='option55'
+                                autoComplete='off'
+                            />
+                            <label htmlFor="option55" className='btn'>
+                                <span style={{ backgroundColor: "#891bf0" }}></span>
+                                بنفش
+                            </label>
+
+                            <input type="radio" className='btn-check' name='options' id='option66'
+                                autoComplete='off'
+                            />
+                            <label htmlFor="option66" className='btn'>
+                                <span style={{ backgroundColor: "#f0501b" }}></span>
+                                نارنجی
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div className={`${styles.filter_item} text-center`}>
+                    <a href="" className={styles.btn_outline_site}>
+                        اعمال فیلتر
+                    </a>
+                </div>
             </div>
         </div>
     )
 }
 
-export default SearchFilters;
+export default SearchFilters
