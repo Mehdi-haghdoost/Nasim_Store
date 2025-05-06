28b998b99795dbcd0d3ee1c845d9a8d27c0e36d9
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2025-05-06 12:17:53 +0330

    Updated user.types.js for user GraphQL types

--- src/graphql/server/types/user.types.js
+++ src/graphql/server/types/user.types.js
@@ -1,15 +1,22 @@
-const { GraphQLID, GraphQLString, GraphQLList, GraphQLObjectType, GraphQLNonNull, GraphQLInputObjectType } = require('graphql')
+const {
+    GraphQLID,
+    GraphQLString,
+    GraphQLList,
+    GraphQLObjectType,
+    GraphQLNonNull,
+    GraphQLInputObjectType,
+} = require('graphql');
+
 const CommentModel = require("../../../../models/Comment");
 const AddressModel = require('../../../../models/Address');
 const { CommentType } = require('./comment.types');
 const TicketModel = require('../../../../models/Ticket');
 
-
 const UserType = new GraphQLObjectType({
     name: "UserType",
     fields: () => {
-     
         const { TicketType } = require('./ticket.types');
+        const { AddressType } = require('./address.types');
 
         return {
             _id: { type: GraphQLID },
@@ -20,24 +27,21 @@ const UserType = new GraphQLObjectType({
             nationalId: { type: GraphQLString },
             postalCode: { type: GraphQLString },
             bio: { type: GraphQLString },
-            address: {
-                type: new GraphQLList(GraphQLString),
-                    resolve: async (parent) => {
-                        try {
-                            if (!parent.address || parent.address.length === 0) {
-                                return [];
-                            }
-                            // اگر آدرس‌ها قبلاً populate شده‌اند
-                            if (typeof parent.addresses[0] !== 'string' && parent.addresses[0]._id) {
-                                return parent.addresses;
-                            }
-                            const addresses = await AddressModel.find({ _id: { $in: parent.address } });
-                            return addresses;
-                        } catch (error) {
-    
-                            throw new Error(`خطا در بازیابی آدرس‌ها: ${error.message}`);
+            addresses: {
+                type: new GraphQLList(AddressType),
+                resolve: async (parent) => {
+                    try {
+                        if (!parent.addresses || parent.addresses.length === 0) {
+                            return [];
                         }
+                        const addresses = await AddressModel.find({
+                            _id: { $in: parent.addresses },
+                        });
+                        return addresses;
+                    } catch (error) {
+                        throw new Error(`خطا در بازیابی آدرس‌ها: ${error.message}`);
                     }
+                },
             },
             wishlist: { type: new GraphQLList(GraphQLString) },
             cart: { type: new GraphQLList(GraphQLString) },
@@ -47,39 +51,45 @@ const UserType = new GraphQLObjectType({
             dateOfBirth: { type: GraphQLString },
             comments: {
                 type: new GraphQLList(CommentType),
-                    resolve: async (parent) => {
-                        try {
-                            if (!parent.comments || parent.comments.length === 0) {
-                                return [];
-                            }
-    
-                            const comments = await CommentModel.find({ _id: { $in: parent.comments } });
-                            return comments;
-                        } catch (error) {
-                            console.log(`خطا در بازیبای کامنت ها ${error.message}`);
-                            throw new Error(`خطا در بازیابی کامنت‌ها. لطفاً دوباره تلاش کنید.${error.message}`);
+                resolve: async (parent) => {
+                    try {
+                        if (!parent.comments || parent.comments.length === 0) {
+                            return [];
                         }
+                        const comments = await CommentModel.find({
+                            _id: { $in: parent.comments },
+                        });
+                        return comments;
+                    } catch (error) {
+                        console.log(`خطا در بازیبای کامنت‌ها ${error.message}`);
+                        throw new Error(
+                            `خطا در بازیابی کامنت‌ها. لطفاً دوباره تلاش کنید. ${error.message}`
+                        );
                     }
+                },
             },
             tickets: {
                 type: new GraphQLList(TicketType),
-                    resolve: async (parent) => {
-                        try {
-                            if (!parent.tickets || parent.tickets.length === 0) {
-                                return [];
-                            }
-    
-                            const tickets = await TicketModel.find({ _id: { $in: parent.tickets } });
-                            return tickets;
-                        } catch (error) {
-                            throw new Error(`خطا در بازیابی تیکت‌ها. لطفاً دوباره تلاش کنید.${error.message}`);
+                resolve: async (parent) => {
+                    try {
+                        if (!parent.tickets || parent.tickets.length === 0) {
+                            return [];
                         }
+                        const tickets = await TicketModel.find({
+                            _id: { $in: parent.tickets },
+                        });
+                        return tickets;
+                    } catch (error) {
+                        throw new Error(
+                            `خطا در بازیابی تیکت‌ها. لطفاً دوباره تلاش کنید. ${error.message}`
+                        );
                     }
+                },
             },
             createdAt: { type: GraphQLString },
             updatedAt: { type: GraphQLString },
-        }
-    }
+        };
+    },
 });
 
 const AuthType = new GraphQLObjectType({
@@ -88,18 +98,16 @@ const AuthType = new GraphQLObjectType({
         token: { type: GraphQLString },
         refreshToken: { type: GraphQLString },
         user: { type: UserType },
-    }
-})
+    },
+});
 
 const OtpType = new GraphQLObjectType({
     name: "OtpType",
     fields: {
         message: { type: GraphQLString },
-    }
-})
+    },
+});
 
-
-// تعریف تایپ ورودی برای به‌روزرسانی پروفایل کاربر
 const UserProfileInputType = new GraphQLInputObjectType({
     name: "UserProfileInput",
     fields: {
@@ -109,7 +117,7 @@ const UserProfileInputType = new GraphQLInputObjectType({
         nationalId: { type: GraphQLString },
         postalCode: { type: GraphQLString },
         bio: { type: GraphQLString },
-    }
+    },
 });
 
 module.exports = { UserType, AuthType, OtpType, UserProfileInputType };
