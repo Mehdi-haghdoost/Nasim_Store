e6a00448eb07e4789c11aef3a9f679575cb69ffb
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2025-05-10 03:12:27 +0330

    Improve login-register page with loading state
    - Added loading state handling to prevent flashing of login/register page for authenticated users.
    - Fixed redirect logic to ensure seamless navigation to the homepage.

--- src/app/login-register/page.js
+++ src/app/login-register/page.js
@@ -1,22 +1,57 @@
 "use client";
-import React, { useState } from 'react';
-import Login from '@/components/templates/login-register/Login'
-import Register from '@/components/templates/login-register/Register'
+import React, { useState, useEffect } from "react";
+import Login from "@/components/templates/login-register/Login";
+import Register from "@/components/templates/login-register/Register";
 import { authTypes } from "../../utils/constans";
+import { useAuth } from "@/Redux/hooks/useAuth";
+import { useRouter } from "next/navigation";
+import { useDispatch } from "react-redux";
+import { checkAuth } from "@/Redux/actions/authThunks";
 
-function page() {
-  const [authtype,setAuthType] = useState(authTypes.LOGIN);
+function Page() {
+  const [authType, setAuthType] = useState(authTypes.LOGIN);
+  const [initialLoading, setInitialLoading] = useState(true); // وضعیت لودینگ اولیه
+  const { isAuthenticated, loading } = useAuth();
+  const router = useRouter();
+  const dispatch = useDispatch();
+
+  // بررسی اولیه احراز هویت با اولویت بالا
+  useEffect(() => {
+    dispatch(checkAuth());
+  }, [dispatch]);
+
+  // ریدایرکت در صورت احراز هویت
+  useEffect(() => {
+    if (!loading) {
+      if (isAuthenticated) {
+        router.replace("/");
+      } else {
+        setInitialLoading(false); // اگر کاربر لاگین نیست، لودینگ را متوقف کن
+      }
+    }
+  }, [isAuthenticated, loading, router]);
+
   const showRegisterForm = () => setAuthType(authTypes.REGISTER);
   const showLoginForm = () => setAuthType(authTypes.LOGIN);
+
+  // نمایش لودینگ تا زمانی که وضعیت احراز هویت مشخص شود
+  if (initialLoading) {
+    return (
+      <div className="flex justify-center items-center min-h-screen">
+        <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-gray-900"></div>
+      </div>
+    );
+  }
+
   return (
     <>
-    {authtype === authTypes.LOGIN ? (
-      <Login showRegisterForm={showRegisterForm} />
-    ) : (
-      <Register showLoginForm={showLoginForm} />
-    )}
+      {authType === authTypes.LOGIN ? (
+        <Login showRegisterForm={showRegisterForm} />
+      ) : (
+        <Register showLoginForm={showLoginForm} />
+      )}
     </>
-  )
+  );
 }
 
-export default page
+export default Page;
