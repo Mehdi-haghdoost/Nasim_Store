36b0b01210e29c197310b1180300317391fa1656
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2025-05-10 06:06:49 +0330

    Enhance useComment hook with fetchUserComments functionality

--- src/Redux/hooks/useComment.js
+++ src/Redux/hooks/useComment.js
@@ -1,5 +1,5 @@
 import { useSelector, useDispatch } from "react-redux";
-import { replyToComment, addComment } from "../actions/commentThunks";
+import { replyToComment, addComment, getUserComments } from "../actions/commentThunks";
 import { openReplyForm, closeReplyForm, clearReplyStatus, clearAddStatus } from "../slices/commentSlice";
 
 export const useComment = () => {
@@ -9,9 +9,14 @@ export const useComment = () => {
   const submitReply = (replyData) => {
     return dispatch(replyToComment(replyData));
   };
-  
+
   const submitComment = (commentData) => {
     return dispatch(addComment(commentData));
+  };
+
+
+  const fetchUserComments = (page = 1, limit = 10) => {
+    return dispatch(getUserComments({ page, limit }));
   };
 
   return {
@@ -24,13 +29,23 @@ export const useComment = () => {
     openReplyForm: (parentId) => dispatch(openReplyForm(parentId)),
     closeReplyForm: () => dispatch(closeReplyForm()),
     clearReplyStatus: () => dispatch(clearReplyStatus()),
-    
+
     // وضعیت و اکشن‌های کامنت اصلی
     addLoading: commentState.addLoading,
     addError: commentState.addError,
     addSuccess: commentState.addSuccess,
     submitComment,
-    clearAddStatus: () => dispatch(clearAddStatus())
+    clearAddStatus: () => dispatch(clearAddStatus()),
+
+
+    // کامنت‌های کاربر
+    userComments: commentState.userComments,
+    totalPages: commentState.totalPages,
+    currentPage: commentState.currentPage,
+    totalComments: commentState.totalComments,
+    userCommentsLoading: commentState.userCommentsLoading,
+    userCommentsError: commentState.userCommentsError,
+    fetchUserComments,
   };
 };
 
