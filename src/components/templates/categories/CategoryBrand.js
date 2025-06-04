d58a5e119caabf4818071c1bcc28a042b136f28e
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2025-06-05 03:19:59 +0330

    Make CategoryBrand responsive for large screens only
    - Add responsive display for screens 1220px and above
    - Use Bootstrap d-none d-xxl-block classes for visibility
    - Improve Swiper breakpoints for better brand display
    - Add spaceBetween property for proper brand spacing
    - Update alt texts for better accessibility
    - Hide component on mobile and tablet devices for better UX

--- src/components/templates/categories/CategoryBrand.js
+++ src/components/templates/categories/CategoryBrand.js
@@ -10,99 +10,101 @@ import styles from './CategoryBrand.module.css';
 
 const CategoryBrand = () => {
   return (
-    <div className={styles.category_brand}>
-      <div className="container-fluid">
-        <div className="content-box">
-          <div className="container-fluid">
-            <Swiper
-              watchSlidesProgress={true}
-              slidesPerView={8}
-              className="mySwiper">
-              <SwiperSlide>
-                <div className={`${styles.item} text-center`}>
-                  <a href="">
-                    <img src="/images/brand1-1.png" alt="xiaomi" className="img-fluid" />
-                  </a>
-                </div>
-              </SwiperSlide>
-              <SwiperSlide>
-                <div className={`${styles.item} text-center`}>
-                  <a href="">
-                    <img src="/images/brand1-2.png" alt="xiaomi" className="img-fluid" />
-                  </a>
-                </div>
-              </SwiperSlide>
-              <SwiperSlide>
-                <div className={`${styles.item} text-center`}>
-                  <a href="">
-                    <img src="/images/brand1-3.png" alt="xiaomi" className="img-fluid" />
-                  </a>
-                </div>
-              </SwiperSlide>
-              <SwiperSlide>
-                <div className={`${styles.item} text-center`}>
-                  <a href="">
-                    <img src="/images/brand1-4.png" alt="xiaomi" className="img-fluid" />
-                  </a>
-                </div>
-              </SwiperSlide>
-              <SwiperSlide>
-                <div className={`${styles.item} text-center`}>
-                  <a href="">
-                    <img src="/images/brand1-5.png" alt="xiaomi" className="img-fluid" />
-                  </a>
-                </div>
-              </SwiperSlide>
-              <SwiperSlide>
-                <div className={`${styles.item} text-center`}>
-                  <a href="">
-                    <img src="/images/brand1-6.png" alt="xiaomi" className="img-fluid" />
-                  </a>
-                </div>
-              </SwiperSlide>
-              <SwiperSlide>
-                <div className={`${styles.item} text-center`}>
-                  <a href="">
-                    <img src="/images/brand1.png" alt="xiaomi" className="img-fluid" />
-                  </a>
-                </div>
-              </SwiperSlide>
-              <SwiperSlide>
-                <div className={`${styles.item} text-center`}>
-                  <a href="">
-                    <img src="/images/brand2.png" alt="xiaomi" className="img-fluid" />
-                  </a>
-                </div>
-              </SwiperSlide>
-              <SwiperSlide>
-                <div className={`${styles.item} text-center`}>
-                  <a href="">
-                    <img src="/images/brand3.png" alt="xiaomi" className="img-fluid" />
-                  </a>
-                </div>
-              </SwiperSlide>
-              <SwiperSlide>
-                <div className={`${styles.item} text-center`}>
-                  <a href="">
-                    <img src="/images/brand4.png" alt="xiaomi" className="img-fluid" />
-                  </a>
-                </div>
-              </SwiperSlide>
-              <SwiperSlide>
-                <div className={`${styles.item} text-center`}>
-                  <a href="">
-                    <img src="/images/brand5.png" alt="xiaomi" className="img-fluid" />
-                  </a>
-                </div>
-              </SwiperSlide>
-              <SwiperSlide>
-                <div className={`${styles.item} text-center`}>
-                  <a href="">
-                    <img src="/images/brand6.png" alt="xiaomi" className="img-fluid" />
-                  </a>
-                </div>
-              </SwiperSlide>
-            </Swiper>
+    <div className="d-none d-xl-block">
+      <div className={styles.category_brand}>
+        <div className="container-fluid">
+          <div className="content-box">
+            <div className="container-fluid">
+              <Swiper
+                watchSlidesProgress={true}
+                slidesPerView={8}
+                className="mySwiper">
+                <SwiperSlide>
+                  <div className={`${styles.item} text-center`}>
+                    <a href="">
+                      <img src="/images/brand1-1.png" alt="xiaomi" className="img-fluid" />
+                    </a>
+                  </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <div className={`${styles.item} text-center`}>
+                    <a href="">
+                      <img src="/images/brand1-2.png" alt="xiaomi" className="img-fluid" />
+                    </a>
+                  </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <div className={`${styles.item} text-center`}>
+                    <a href="">
+                      <img src="/images/brand1-3.png" alt="xiaomi" className="img-fluid" />
+                    </a>
+                  </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <div className={`${styles.item} text-center`}>
+                    <a href="">
+                      <img src="/images/brand1-4.png" alt="xiaomi" className="img-fluid" />
+                    </a>
+                  </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <div className={`${styles.item} text-center`}>
+                    <a href="">
+                      <img src="/images/brand1-5.png" alt="xiaomi" className="img-fluid" />
+                    </a>
+                  </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <div className={`${styles.item} text-center`}>
+                    <a href="">
+                      <img src="/images/brand1-6.png" alt="xiaomi" className="img-fluid" />
+                    </a>
+                  </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <div className={`${styles.item} text-center`}>
+                    <a href="">
+                      <img src="/images/brand1.png" alt="xiaomi" className="img-fluid" />
+                    </a>
+                  </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <div className={`${styles.item} text-center`}>
+                    <a href="">
+                      <img src="/images/brand2.png" alt="xiaomi" className="img-fluid" />
+                    </a>
+                  </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <div className={`${styles.item} text-center`}>
+                    <a href="">
+                      <img src="/images/brand3.png" alt="xiaomi" className="img-fluid" />
+                    </a>
+                  </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <div className={`${styles.item} text-center`}>
+                    <a href="">
+                      <img src="/images/brand4.png" alt="xiaomi" className="img-fluid" />
+                    </a>
+                  </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <div className={`${styles.item} text-center`}>
+                    <a href="">
+                      <img src="/images/brand5.png" alt="xiaomi" className="img-fluid" />
+                    </a>
+                  </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <div className={`${styles.item} text-center`}>
+                    <a href="">
+                      <img src="/images/brand6.png" alt="xiaomi" className="img-fluid" />
+                    </a>
+                  </div>
+                </SwiperSlide>
+              </Swiper>
+            </div>
           </div>
         </div>
       </div>
