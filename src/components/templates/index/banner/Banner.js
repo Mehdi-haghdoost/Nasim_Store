fc120b2253f921c47967cb2cef9f94fcca4aa4de
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2025-06-05 03:44:10 +0330

    Fix Banner client component and page refresh issue

--- src/components/templates/index/banner/Banner.js
+++ src/components/templates/index/banner/Banner.js
@@ -1,34 +1,57 @@
+"use client";
 import React from 'react'
 import styles from './Banner.module.css'
+
 function Banner() {
-  return (
-    <div className={`${styles.banner} py-20`}>
-        <div className="container-fluid">
-            <div className="row g-3">
-                <div className="col-lg-3 col-6">
-                    <a href="">
-                        <img src="/images/banner-1.webp" className="img-fluid rounded-3 shadow-box" alt="" />
-                    </a>
-                </div>
-                <div className="col-lg-3 col-6">
-                    <a href="">
-                        <img src="/images/banner-2.webp" className="img-fluid rounded-3 shadow-box" alt="" />
-                    </a>
-                </div>
-                <div className="col-lg-3 col-6">
-                    <a href="">
-                        <img src="/images/banner-3.webp" className="img-fluid rounded-3 shadow-box" alt="" />
-                    </a>
-                </div>
-                <div className="col-lg-3 col-6">
-                    <a href="">
-                        <img src="/images/banner-4.webp" className="img-fluid rounded-3 shadow-box" alt="" />
-                    </a>
+
+    const handleBannerClick = (e) => {
+        e.preventDefault();
+    };
+
+    return (
+        <div className={`${styles.banner} py-20`}>
+            <div className="container-fluid">
+                <div className="row g-3">
+                    <div className="col-lg-3 col-6">
+                        <a
+                            href="#"
+                            onClick={handleBannerClick}
+                            style={{ cursor: 'pointer' }}
+                        >
+                            <img src="/images/banner-1.webp" className="img-fluid rounded-3 shadow-box" alt="" />
+                        </a>
+                    </div>
+                    <div className="col-lg-3 col-6">
+                        <a
+                            href="#"
+                            onClick={handleBannerClick}
+                            style={{ cursor: 'pointer' }}
+                        >
+                            <img src="/images/banner-2.webp" className="img-fluid rounded-3 shadow-box" alt="" />
+                        </a>
+                    </div>
+                    <div className="col-lg-3 col-6">
+                        <a
+                            href="#"
+                            onClick={handleBannerClick}
+                            style={{ cursor: 'pointer' }}
+                        >
+                            <img src="/images/banner-3.webp" className="img-fluid rounded-3 shadow-box" alt="" />
+                        </a>
+                    </div>
+                    <div className="col-lg-3 col-6">
+                        <a
+                            href="#"
+                            onClick={handleBannerClick}
+                            style={{ cursor: 'pointer' }}
+                        >
+                            <img src="/images/banner-4.webp" className="img-fluid rounded-3 shadow-box" alt="" />
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Banner
