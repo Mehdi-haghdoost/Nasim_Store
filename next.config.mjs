f75313ec1091b50d42cd5b45dc81049867ec39d5
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2025-06-05 08:46:21 +0330

    Update next.config.mjs to exclude Express server from Vercel build

--- next.config.mjs
+++ next.config.mjs
@@ -7,12 +7,37 @@
 
 // export default nextConfig;
 
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     experimental: {
+//         appDir: true,
+//     },
+//     reactStrictMode: false, // غیرفعال کردن StrictMode
+// };
+
+// export default nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
         appDir: true,
+        serverComponentsExternalPackages: ['mongoose'],
     },
-    reactStrictMode: false, // غیرفعال کردن StrictMode
+    reactStrictMode: false,
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            config.resolve.alias = {
+                ...config.resolve.alias,
+                '@/graphql/server/graphql': false,
+            };
+        }
+        return config;
+    },
+
+    outputFileTracingExcludes: {
+        '*': [
+            'src/graphql/server/graphql.js',
+        ],
+    },
 };
 
 export default nextConfig;
