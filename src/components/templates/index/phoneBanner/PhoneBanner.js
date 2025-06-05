a2b62295aa63fc7a63510598fbb2c4195d145a9d
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2025-06-05 03:40:27 +0330

    Fix PhoneBanner client component and page refresh issue
    - Add 'use client' directive for Next.js client component
    - Fix event handlers cannot be passed to Client Component error
    - Add preventDefault to banner click handlers
    - Prevent page refresh when clicking on banner links
    - Add proper cursor pointer styling for better UX
    - Improve alt texts for banner images accessibility

--- src/components/templates/index/phoneBanner/PhoneBanner.js
+++ src/components/templates/index/phoneBanner/PhoneBanner.js
@@ -1,28 +1,59 @@
+"use client";
+
 import React from 'react'
 import styles from './PhoneBanner.module.css'
+
 function PhoneBanner() {
+  const handleBannerClick = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className={`${styles.banner} py-20`}>
-    <div className="container-fluid">
+      <div className="container-fluid">
         <div className="row g-3">
-            <div className="col-sm-4">
-                <a href="">
-                    <img src="/images/banner-11.jpg" className="img-fluid rounded-3 shadow-box" alt="" />
-                </a>
-            </div>
-            <div className="col-sm-4">
-                <a href="">
-                    <img src="/images/banner-12.jpg" className="img-fluid rounded-3 shadow-box" alt="" />
-                </a>
-            </div>
-            <div className="col-sm-4">
-                <a href="">
-                    <img src="/images/banner-13.jpg" className="img-fluid rounded-3 shadow-box" alt="" />
-                </a>
-            </div>
+          <div className="col-sm-4">
+            <a 
+              href="#"
+              onClick={handleBannerClick}
+              style={{ cursor: 'pointer' }}
+            >
+              <img 
+                src="/images/banner-11.jpg" 
+                className="img-fluid rounded-3 shadow-box" 
+                alt="بنر تبلیغاتی" 
+              />
+            </a>
+          </div>
+          <div className="col-sm-4">
+            <a 
+              href="#"
+              onClick={handleBannerClick}
+              style={{ cursor: 'pointer' }}
+            >
+              <img 
+                src="/images/banner-12.jpg" 
+                className="img-fluid rounded-3 shadow-box" 
+                alt="بنر تبلیغاتی" 
+              />
+            </a>
+          </div>
+          <div className="col-sm-4">
+            <a 
+              href="#"
+              onClick={handleBannerClick}
+              style={{ cursor: 'pointer' }}
+            >
+              <img 
+                src="/images/banner-13.jpg" 
+                className="img-fluid rounded-3 shadow-box" 
+                alt="بنر تبلیغاتی" 
+              />
+            </a>
+          </div>
         </div>
+      </div>
     </div>
-</div>
   )
 }
 
