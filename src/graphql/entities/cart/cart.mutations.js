d98dd5868556d2899711d960549fdb637ca83be8
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2025-06-10 13:54:32 +0330

    feat: create cart GraphQL entities for client-side

--- src/graphql/entities/cart/cart.mutations.js
+++ src/graphql/entities/cart/cart.mutations.js
@@ -1,51 +1,185 @@
+// import { gql } from '@apollo/client';
+
+// // Mutation برای افزودن محصول به سبد خرید
+// // مطابق با resolver addToCart
+// export const ADD_TO_CART = gql`
+//   mutation AddToCart($input: AddToCartInput!) {
+//     addToCart(input: $input) {
+//       _id
+//       product {
+//         _id
+//         title
+//         image
+//         price
+//         discountedPrice
+//         hasDiscount
+//         stock
+//       }
+//       quantity
+//       color
+//       size
+//       selectedSeller {
+//         _id
+//         name
+//       }
+//     }
+//   }
+// `;
+
+// // Mutation برای به‌روزرسانی تعداد محصول در سبد خرید
+// // مطابق با resolver updateCartItem
+// export const UPDATE_CART_ITEM = gql`
+//   mutation UpdateCartItem($input: UpdateCartItemInput!) {
+//     updateCartItem(input: $input)
+//   }
+// `;
+
+// // Mutation برای حذف محصول از سبد خرید
+// // مطابق با resolver removeFromCart
+// export const REMOVE_FROM_CART = gql`
+//   mutation RemoveFromCart($itemId: ID!) {
+//     removeFromCart(itemId: $itemId)
+//   }
+// `;
+
+// // Mutation برای پاک کردن کامل سبد خرید
+// // مطابق با resolver clearCart
+// export const CLEAR_CART = gql`
+//   mutation ClearCart {
+//     clearCart
+//   }
+// `;
+
 import { gql } from '@apollo/client';
 
-// Mutation برای افزودن محصول به سبد خرید
-// مطابق با resolver addToCart
+// افزودن محصول به سبد خرید کاربر لاگین شده
 export const ADD_TO_CART = gql`
   mutation AddToCart($input: AddToCartInput!) {
     addToCart(input: $input) {
-      _id
-      product {
+      items {
         _id
-        title
-        image
-        price
-        discountedPrice
-        hasDiscount
-        stock
+        product {
+          _id
+          title
+          image
+          price
+          discountedPrice
+          hasDiscount
+          stock
+          brandIcon
+        }
+        quantity
+        color
+        size
+        selectedSeller {
+          _id
+          name
+          logo
+        }
+        addedAt
       }
-      quantity
-      color
-      size
-      selectedSeller {
-        _id
-        name
-      }
+      totalPrice
+      totalDiscount
+      finalPrice
+      lastModified
     }
   }
 `;
 
-// Mutation برای به‌روزرسانی تعداد محصول در سبد خرید
-// مطابق با resolver updateCartItem
+// به‌روزرسانی تعداد محصول در سبد خرید
 export const UPDATE_CART_ITEM = gql`
   mutation UpdateCartItem($input: UpdateCartItemInput!) {
-    updateCartItem(input: $input)
+    updateCartItem(input: $input) {
+      items {
+        _id
+        product {
+          _id
+          title
+          image
+          price
+          discountedPrice
+          hasDiscount
+          stock
+        }
+        quantity
+        color
+        size
+        selectedSeller {
+          _id
+          name
+        }
+      }
+      totalPrice
+      totalDiscount
+      finalPrice
+      lastModified
+    }
   }
 `;
 
-// Mutation برای حذف محصول از سبد خرید
-// مطابق با resolver removeFromCart
+// حذف محصول از سبد خرید
 export const REMOVE_FROM_CART = gql`
   mutation RemoveFromCart($itemId: ID!) {
-    removeFromCart(itemId: $itemId)
+    removeFromCart(itemId: $itemId) {
+      items {
+        _id
+        product {
+          _id
+          title
+          image
+          price
+          discountedPrice
+          hasDiscount
+        }
+        quantity
+        color
+        size
+      }
+      totalPrice
+      totalDiscount
+      finalPrice
+      lastModified
+    }
   }
 `;
 
-// Mutation برای پاک کردن کامل سبد خرید
-// مطابق با resolver clearCart
+// پاک کردن کامل سبد خرید
 export const CLEAR_CART = gql`
   mutation ClearCart {
     clearCart
+  }
+`;
+
+// سینک کردن سبد مهمان با سبد کاربر
+export const SYNC_GUEST_CART = gql`
+  mutation SyncGuestCart($input: SyncCartInput!) {
+    syncGuestCart(input: $input) {
+      items {
+        _id
+        product {
+          _id
+          title
+          image
+          price
+          discountedPrice
+          hasDiscount
+          stock
+          brandIcon
+        }
+        quantity
+        color
+        size
+        selectedSeller {
+          _id
+          name
+          logo
+        }
+        addedAt
+      }
+      totalPrice
+      totalDiscount
+      finalPrice
+      lastModified
+    }
   }
 `;
--- src/graphql/entities/cart/cart.queries.js
+++ src/graphql/entities/cart/cart.queries.js
@@ -1,7 +1,39 @@
+// import { gql } from '@apollo/client';
+
+// // Query برای دریافت سبد خرید کاربر
+// // این مطابق با resolver getUserCart است
+// export const GET_USER_CART = gql`
+//   query GetUserCart {
+//     getUserCart {
+//       items {
+//         _id
+//         product {
+//           _id
+//           title
+//           image
+//           price
+//           discountedPrice
+//           hasDiscount
+//           stock
+//         }
+//         quantity
+//         color
+//         size
+//         selectedSeller {
+//           _id
+//           name
+//         }
+//       }
+//       totalPrice
+//       totalDiscount
+//       finalPrice
+//     }
+//   }
+// `;
+
 import { gql } from '@apollo/client';
 
-// Query برای دریافت سبد خرید کاربر
-// این مطابق با resolver getUserCart است
+// دریافت سبد خرید کاربر لاگین شده
 export const GET_USER_CART = gql`
   query GetUserCart {
     getUserCart {
@@ -15,6 +47,17 @@ export const GET_USER_CART = gql`
           discountedPrice
           hasDiscount
           stock
+          brandIcon
+          category {
+            _id
+            name
+          }
+          sellers {
+            _id
+            name
+            logo
+            rating
+          }
         }
         quantity
         color
@@ -22,11 +65,15 @@ export const GET_USER_CART = gql`
         selectedSeller {
           _id
           name
+          logo
+          rating
         }
+        addedAt
       }
       totalPrice
       totalDiscount
       finalPrice
+      lastModified
     }
   }
 `;
